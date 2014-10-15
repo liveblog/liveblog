792b5daa9ec3dd06b9a23fc10d6a9e21035b9502
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-10-15 14:38:39 +0300

    chore(karma): add needed files
    LBSD-167

---
+++ karma.conf.js
@@ -0,0 +1,57 @@
+'use strict';
+
+module.exports = function(config) {
+    config.set({
+        frameworks: [
+            'jasmine',
+            'requirejs'
+        ],
+
+        preprocessors: {
+            '**/*.html': ['ng-html2js']
+        },
+
+        // list of files / patterns to load in the browser
+        files: [
+            'app/scripts/bower_components/jquery/dist/jquery.js',
+            'app/scripts/bower_components/angular/angular.js',
+            {pattern: 'app/**/*.js', included: false},
+            {pattern: 'app/scripts/liveblog-*/**/*[sS]pec.js', included: false},
+            'app/scripts/liveblog-*/**/views/*.html',
+            'test-main.js'
+        ],
+
+        // list of files to exclude
+        exclude: [
+            'app/scripts/bower_components/**/*[sS]pec.js',
+            'app/main.js'
+        ],
+
+        ngHtml2JsPreprocessor: {
+            prependPrefix: '/base/',
+            moduleName: 'templates'
+        },
+
+        junitReporter: {
+            outputFile: 'test-results.xml'
+        },
+
+        // test results reporter to use
+        reporters: ['dots'],
+
+        // web server port
+        port: 8080,
+
+        // cli runner port
+        runnerPort: 9100,
+
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+        // Start these browsers, currently available:
+        browsers: ['Chrome'],
+
+        // Continuous Integration mode
+        singleRun: false
+    });
+};
