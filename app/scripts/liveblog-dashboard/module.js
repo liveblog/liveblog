df53e23bf7b299d1092fe9661f26d3af7685d9d9
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-10-27 15:49:39 +0200

    refactor(liveblog-dashboard): depends directly from superdesk-dashboard
    LBSD-133
    
    Conflicts:
    	app/index.html

--- app/scripts/liveblog-dashboard/module.js
+++ app/scripts/liveblog-dashboard/module.js
@@ -1,40 +1,17 @@
 define([
     'angular',
     'require',
-    './workspace-controller',
-    './workspace-service',
-    './sd-widget-directive',
-    './widgets-provider',
-    './grid/grid',
-    './world-clock/world-clock'
+    'superdesk-dashboard/workspace-controller'
 ], function(angular, require) {
     'use strict';
 
-    // to avoid circular dependency
-    angular.module('liveblog.dashboard.widgets', []).
-        provider('widgets', require('./widgets-provider'));
-
-    return angular.module('liveblog.dashboard', [
-        'liveblog.dashboard.widgets',
-        'liveblog.dashboard.grid',
-        'liveblog.dashboard.world-clock',
-        'superdesk.workspace.content'
-    ])
-
-    .service('workspace', require('./workspace-service'))
-    .directive('sdWidget', require('./sd-widget-directive'))
-
-    .filter('wcodeFilter', function() {
-        return function(input, values) {
-            return _.pick(input, _.difference(_.keys(input), _.keys(values)));
-        };
-    })
+    return angular.module('liveblog.dashboard', ['superdesk.dashboard'])
 
     .config(['superdeskProvider', function(superdesk) {
         superdesk.activity('/workspace', {
             label: gettext('Workspace'),
-            controller: require('./workspace-controller'),
-            templateUrl: require.toUrl('./views/workspace.html'),
+            controller: require('superdesk-dashboard/workspace-controller'),
+            templateUrl: require.toUrl('superdesk-dashboard/views/workspace.html'),
             topTemplateUrl: require.toUrl('./views/workspace-topnav.html'),
             priority: -1000,
             category: superdesk.MENU_MAIN
