5ac87d6d43d2e7911730069961243ca3b7e22aba
actionless <actionless.loveless@gmail.com>
2014-11-10 10:42:13 +0100

    fix(tests): removed trailing slash, added e2e test results dir to .gitignore

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -3,7 +3,7 @@
 #node_modules/.bin/protractor protractor-conf.js \
 protractor protractor-conf.js \
   --stackTrace --verbose \
-  --params.baseUrl 'https://liveblog.sd-test.sourcefabric.org/' \
-  --params.baseBackendUrl 'https://liveblog.sd-test.sourcefabric.org/api/' \
+  --params.baseUrl 'https://petr.sd-test.sourcefabric.org' \
+  --params.baseBackendUrl 'https://petr.sourcefabric.org/api' \
   --params.username 'admin' \
   --params.password 'admin'
