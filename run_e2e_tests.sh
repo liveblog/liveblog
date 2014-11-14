2bba302d5136a6a2ca75d70ce887f1249b41cd33
actionless <actionless.loveless@gmail.com>
2014-11-14 13:46:31 +0100

    fix(e2e): prepopulation payload; error reporting on bad auth; reverted removed workaround for page reload

--- run_e2e_tests.sh
+++ run_e2e_tests.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 #protractor protractor-conf.js \
+#  --specs "spec/setup.js,spec/matchers.js,spec/login_spec.js" \
 ./node_modules/.bin/protractor protractor-conf.js \
   --stackTrace --verbose \
-  --baseUrl 'https://master.sd-test.sourcefabric.org' \
-  --params.baseBackendUrl 'https://master.sd-test.sourcefabric.org/api' \
+  --baseUrl 'https://liveblog.sd-test.sourcefabric.org' \
+  --params.baseBackendUrl 'https://liveblog.sd-test.sourcefabric.org/api' \
   --params.username 'admin' \
   --params.password 'admin'
