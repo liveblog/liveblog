1d74b938056cd49f62daa4878ad65d64543a6bd7
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2014-12-05 15:51:24 +0200

    chore(update): update superdesk-client dependency.

--- test-main.js
+++ test-main.js
@@ -13,18 +13,58 @@ for (var file in window.__karma__.files) {
         }
     }
 }
-var paths = {superdesk: 'bower_components/superdesk/app/'};
+var paths = {superdesk: 'bower_components/superdesk/app/scripts/'};
 // we have to put here files tested without requirejs
-tests.push(paths.superdesk + 'scripts/superdesk/menu/menu');
-tests.push(paths.superdesk + 'scripts/superdesk/menu/notifications/notifications');
-tests.push(paths.superdesk + 'scripts/superdesk-authoring/widgets/widgets');
-tests.push(paths.superdesk + 'scripts/superdesk-authoring/comments/comments');
-tests.push(paths.superdesk + 'scripts/superdesk-workspace/content/content');
+// core
+tests.push(paths.superdesk + 'superdesk/mocks');
+tests.push(paths.superdesk + 'superdesk/api/api');
+tests.push(paths.superdesk + 'superdesk/auth/auth');
+tests.push(paths.superdesk + 'superdesk/menu/menu');
+tests.push(paths.superdesk + 'superdesk/config/config');
+tests.push(paths.superdesk + 'superdesk/editor/editor');
+tests.push(paths.superdesk + 'superdesk/notify/notify');
+tests.push(paths.superdesk + 'superdesk/activity/activity');
+tests.push(paths.superdesk + 'superdesk/menu/notifications/notifications');
+tests.push(paths.superdesk + 'superdesk/services/translate');
+tests.push(paths.superdesk + 'superdesk/services/modalService');
+tests.push(paths.superdesk + 'superdesk/services/preferencesService');
+tests.push(paths.superdesk + 'superdesk/features/features');
+tests.push(paths.superdesk + 'superdesk/services/asset');
+tests.push(paths.superdesk + 'superdesk/privileges/privileges');
+tests.push(paths.superdesk + 'superdesk/beta/beta');
+tests.push(paths.superdesk + 'superdesk/services/storage');
+
+// apps
+tests.push(paths.superdesk + 'superdesk-authoring/authoring');
+tests.push(paths.superdesk + 'superdesk-authoring/widgets/widgets');
+tests.push(paths.superdesk + 'superdesk-authoring/comments/comments');
+tests.push(paths.superdesk + 'superdesk-authoring/workqueue/workqueue');
+tests.push(paths.superdesk + 'superdesk-authoring/metadata/metadata');
+tests.push(paths.superdesk + 'superdesk-authoring/versioning/versions');
+tests.push(paths.superdesk + 'superdesk-workspace/content/content');
+tests.push(paths.superdesk + 'superdesk-desks/module');
+tests.push(paths.superdesk + 'superdesk-groups/groups');
+tests.push(paths.superdesk + 'superdesk-search/search');
+
+tests.push(paths.superdesk + 'superdesk-users/users');
+tests.push(paths.superdesk + 'superdesk-users/profile');
+tests.push(paths.superdesk + 'superdesk-users/activity/activity');
+tests.push(paths.superdesk + 'superdesk-users/import/import');
+
+tests.push(paths.superdesk + 'superdesk-dashboard/module');
+tests.push(paths.superdesk + 'superdesk-dashboard/workspace-tasks/tasks');
+
+tests.push(paths.superdesk + 'superdesk-archive/module');
+
+// libs
 tests.push('bower_components/ment.io/dist/mentio');
+tests.push('angular-gettext');
+tests.push('angular-ui');
+tests.push('angular-route');
 
 requirejs.config({
     baseUrl: '/base/app/scripts',
-    deps: ['angular-mocks', 'gettext'],
+    deps: ['angular-mocks', 'gettext', 'angular'],
 
     callback: function() {
         'use strict';
@@ -39,16 +79,17 @@ requirejs.config({
         lodash: 'bower_components/lodash/dist/lodash',
         d3: 'bower_components/d3/d3',
         'angular-resource': 'bower_components/angular-resource/angular-resource',
+        'angular-gettext': 'bower_components/angular-gettext/dist/angular-gettext',
         'angular-route': 'bower_components/angular-route/angular-route',
         'angular-mocks': 'bower_components/angular-mocks/angular-mocks',
-        'angular-gettext': 'bower_components/angular-gettext/dist/angular-gettext',
+        'angular-ui': 'bower_components/angular-bootstrap/ui-bootstrap',
         'moment-timezone': 'bower_components/moment-timezone/moment-timezone',
 
-        'superdesk': paths.superdesk + 'app/scripts/superdesk',
-        'superdesk-settings': paths.superdesk + 'app/scripts/superdesk-settings',
-        'superdesk-dashboard': paths.superdesk + 'scripts/superdesk-dashboard',
-        'superdesk-users': paths.superdesk + 'scripts/superdesk-users',
-        'superdesk-scratchpad': paths.superdesk + 'scripts/superdesk-scratchpad'
+        'superdesk': paths.superdesk + 'superdesk',
+        'superdesk-settings': paths.superdesk + 'superdesk-settings',
+        'superdesk-dashboard': paths.superdesk + 'superdesk-dashboard',
+        'superdesk-users': paths.superdesk + 'superdesk-users',
+        'superdesk-scratchpad': paths.superdesk + 'superdesk-scratchpad'
     },
 
     shim: {
@@ -62,7 +103,9 @@ requirejs.config({
         },
 
         'angular-resource': ['angular'],
+        'angular-gettext': ['angular'],
         'angular-route': ['angular'],
-        'angular-mocks': ['angular']
+        'angular-mocks': ['angular'],
+        'angular-ui': ['angular']
     }
 });
