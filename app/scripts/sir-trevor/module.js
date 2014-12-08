9355fbd9121c65e6bfbb42ec77ee33f765094324
Edouard <edou4rd@gmail.com>
2014-12-08 11:29:08 +0100

    LBSD-22: sirTrevor directive refactored

--- app/scripts/sir-trevor/module.js
+++ app/scripts/sir-trevor/module.js
@@ -1,20 +1,29 @@
-define([
-    'angular',
-], function(angular) {
+(function(SirTrevor) {
     'use strict';
-    angular.module('sir-trevor', [])
-    .directive('sirTrevor', ['$timeout',  function($timeout) {
+    
+    var sirTrevorDirective = function($timeout) {
+
         var link = function($scope, element) {
             $timeout(function() {
-                new SirTrevor.Editor({ el: element.find('textarea') });
+                var sir_trevor = new SirTrevor.Editor({ el: element.find('textarea') });
             });
         };
         return {
             restrict : 'E',
             link     : link,
-            template : '<form><textarea ng-model="post"></textarea></form>'
+            template : ['<form>',
+                            '<textarea ng-model="post"></textarea>',
+                        '</form>'].join('')
         };
-    }]);
-});
+    };
+
+    define([
+        'angular'
+    ], function(angular) {
+        angular.module('sir-trevor', [])
+            .directive('sirTrevor', ['$timeout',  sirTrevorDirective]);
+    });
+
+})(SirTrevor);
 
 // EOF
