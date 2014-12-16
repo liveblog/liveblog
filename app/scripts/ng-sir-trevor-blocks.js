5f2c22faacc7b485b37478c6b14233884fd190b3
Edouard <edou4rd@gmail.com>
2014-12-16 18:42:29 +0100

    sir-trevor : style for quotations

--- app/scripts/ng-sir-trevor-blocks.js
+++ app/scripts/ng-sir-trevor-blocks.js
@@ -20,10 +20,9 @@ define([
 
                 editorHTML: function() {
                     var template = _.template([
-                        '<blockquote class="st-required st-text-block" contenteditable="true"></blockquote>',
-                        '<label class="st-input-label"> <%= i18n.t("blocks:quote:credit_field") %></label>',
-                        '<input maxlength="140" name="cite" placeholder="<%= i18n.t("blocks:quote:credit_field") %>"',
-                        ' class="st-input-string st-required js-cite-input" type="text" />'
+                        '<blockquote class="st-required st-quote-block quote-input" contenteditable="true"></blockquote>',
+                        '<div contenteditable="true" name="cite" placeholder="<%= i18n.t("blocks:quote:credit_field") %>"',
+                        ' class="st-input-string st-required js-cite-input st-quote-block"></div>'
                     ].join('\n'));
                     return template(this);
                 },
