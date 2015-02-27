6f1f21388596b0a74172bc07acc9c3f6067de983
Edouard <edou4rd@gmail.com>
2015-02-27 17:43:06 +0100

    Draft posts: draft-posts.directive removed and moved to directives.js

--- client/app/scripts/liveblog-edit/directives.js
+++ client/app/scripts/liveblog-edit/directives.js
@@ -11,37 +11,79 @@
 define([
     'angular',
     'lodash',
-    './module'
+    './module',
+    './posts.service'
 ], function(angular, _) {
     'use strict';
-    angular.module('liveblog.edit').directive('slideable', function() {
-        return {
-            restrict: 'A',
-            scope: {
-                'slideableMove': '@',
-                'slideable': '='
-            },
-            link: function(scope, element, attrs) {
-                var old_left = parseInt(element.css('left'), 10);
-                var to_be_moved = angular.element(document.querySelectorAll(scope.slideableMove));
-                var panel_width = element.width();
-                function toggleSlide(new_value, old_value) {
-                    if (scope.slideable) {
-                        element.show();
-                        to_be_moved.css({
-                            left: panel_width + old_left
-                        });
-                    } else {
-                        element.hide();
-                        to_be_moved.css({
-                            left: old_left
-                        });
+
+    // * SLIDEABLE
+    // *  used for left side bar
+    // *   - slideable: take a boolean, true to be opened
+    // *   - slideableMove: take the other element to be right-moved
+    angular.module('liveblog.edit')
+        .directive('slideable', function() {
+            return {
+                restrict: 'A',
+                scope: {
+                    'slideableMove': '@',
+                    'slideable': '='
+                },
+                link: function(scope, element, attrs) {
+                    var old_left = parseInt(element.css('left'), 10);
+                    var to_be_moved = angular.element(document.querySelectorAll(scope.slideableMove));
+                    var panel_width = element.width();
+                    function toggleSlide(new_value, old_value) {
+                        if (scope.slideable) {
+                            element.show();
+                            to_be_moved.css({
+                                left: panel_width + old_left
+                            });
+                        } else {
+                            element.hide();
+                            to_be_moved.css({
+                                left: old_left
+                            });
+                        }
                     }
+                    scope.$watch('slideable', toggleSlide);
                 }
-                scope.$watch('slideable', toggleSlide);
+            };
+        })
+        .directive('draftPosts', ['api', 'postsService',
+            function DraftPostsDirective(api, postsService) {
+                function DraftPostsController($scope, $element) {
+                    // initialize list
+                    postsService.getDrafts($scope.blog._id).then(function (posts) {
+                        $scope.posts = posts;
+                    });
+                    // update list when needed
+                    $scope.$on('lb.posts.updated', function(e, data) {
+                        var posts = _.values(data)[0];
+                        $scope.posts = posts;
+                    });
+                    angular.extend($scope, {
+                        openDraftInEditor: function(draft) {
+                            $scope.editor.reinitialize();
+                            var items = draft.groups[1].refs;
+                            items.forEach(function(item) {
+                                item = item.item;
+                                var data = _.extend({text: item.text}, item.meta);
+                                $scope.editor.createBlock(item.item_type, data);
+                            });
+                        }
+                    });
+                }
+                return {
+                    restrict: 'E',
+                    scope: { // isolated scope
+                        blog: '=',
+                        editor: '='
+                    },
+                    templateUrl: 'scripts/liveblog-edit/views/draft-posts-list.html',
+                    controller: DraftPostsController
+                };
             }
-        };
-    });
+        ]);
 });
 
 // EOF
