be69ac50833d088fc341134d61eff39418af4966
Anca <ancafarcas06@gmail.com>
2015-03-11 12:07:58 +0200

    LBSD-386 backend support for each blog settings + ajusted tests concerning language settings
    Conflicts:
    	server/features/language.feature

---
+++ server/features/language.feature
@@ -0,0 +1,45 @@
+Feature: Language operations
+
+	@auth
+    Scenario: List empty languages
+        Given empty "languages"
+        When we get "/languages"
+        Then we get list with 0 items
+        
+        
+    @auth
+    Scenario: Add language
+        Given empty "languages"
+        When we post to "languages"
+        """
+        [{"language_code": "en"}]
+        """  
+        Then we get existing resource
+        """
+        {"name": "english"}
+        """  
+
+	@auth
+    Scenario: Delete language
+        Given empty "languages"
+        When we post to "languages"
+        """
+        [{"language_code": "fr"}]
+        """
+        When we delete latest
+        Then we get deleted response
+
+	@auth
+    Scenario: Assign a language to a blog
+        Given "languages"
+        """
+        [{"language_code": "fr"}]
+        """
+        When we post to "blogs"
+		"""
+		[{"title": "test_blog1", "language": "#languages._id#"}]
+		"""
+		Then we get existing resource
+		"""
+		{"title": "test_blog1", "language": "#languages._id#"}
+		"""
--- server/liveblog/themes/themes.py
+++ server/liveblog/themes/themes.py
@@ -1,15 +1,15 @@
 from superdesk.resource import Resource
 from superdesk.services import BaseService
 from eve.utils import ParsedRequest
-from settings import SUPPORTED_LANGUAGES
+from settings import SUPPORTED_THEMES
 
 
-class LanguagesResource(Resource):
+class ThemesResource(Resource):
 
     schema = {
-        'language_code': {
+        'name': {
             'type': 'string',
-            'allowed': SUPPORTED_LANGUAGES['languages']
+            'allowed': SUPPORTED_THEMES['themes']
         }
     }
 
@@ -17,18 +17,18 @@ class LanguagesResource(Resource):
         'default_sort': [('_updated', -1)]
     }
 
-    RESOURCE_METHODS = ['GET', 'POST']
     ITEM_METHODS = ['GET', 'POST', 'DELETE']
 
     privileges = {'GET': 'blogs', 'POST': 'blogs', 'PATCH': 'blogs', 'DELETE': 'blogs'}
 
 
-class LanguagesService(BaseService):
+class ThemesService(BaseService):
+
     def on_create(self, docs):
         for doc in docs:
-            doc['name'] = SUPPORTED_LANGUAGES['languages'][doc['language_code']]
+            doc['colour'] = SUPPORTED_THEMES['themes'][doc['name']]
 
     def get(self, req, lookup):
         if req is None:
             req = ParsedRequest()
-        return self.backend.get('languages', req=req, lookup=lookup)
+        return self.backend.get('themes', req=req, lookup=lookup)
