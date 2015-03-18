c93fd94100f46a675c8714db780597b17aa9429d
vladnicoara <vlad.nicoara@sourcefabric.org>
2015-03-18 21:58:24 +0200

    LBSD-416 timeline e2e deletion tests

---
+++ client/spec/timeline_delete_spec.js
@@ -0,0 +1,42 @@
+'use strict';
+var openUrl = require('./helpers/utils').open,
+    openBlog = require('./helpers/utils').openBlog;
+
+describe('timeline deletions', function() {
+    beforeEach(openUrl('/#/liveblog'));
+    it('can delete items on the timeline', function() {
+        openBlog(0);
+        element.all(by.repeater('post in posts')).then(function(posts) {
+            //this post should be single item post
+            posts[2].getText().then(function(txt) {
+                posts[2].element(by.css('.timeline-simple-item')).click();
+                posts[2].element(by.css('[ng-click="askRemoveItem(item, post)"]')).click();
+                confirmRemoval();
+                compareTexts(txt, 2);
+            });
+        });
+    });
+    it('can delete posts on the timeline', function() {
+        openBlog(0);
+        element.all(by.repeater('post in posts')).then(function(posts) {
+            //this post should be a multiple item one
+            posts[1].getText().then(function(txt) {
+                posts[1].element(by.css('.post-item__header')).click();
+                posts[1].element(by.css('[dropdown-toggle]')).click();
+                posts[1].element(by.css('[ng-click="askRemovePost(post)"]')).click();
+                confirmRemoval();
+                compareTexts(txt, 1);
+            });
+        });
+    });
+});
+function compareTexts(oldText, position) {
+    element.all(by.repeater('post in posts')).then(function(newPosts) {
+        expect(oldText).not.toEqual(newPosts[position].getText());
+    });
+}
+
+function confirmRemoval() {
+    browser.wait(element(by.css('[ng-click="ok()"')).isDisplayed);
+    element(by.css('[ng-click="ok()"')).click();
+}
