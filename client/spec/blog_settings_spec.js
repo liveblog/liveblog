e45bfddf8fdf9b9d2214794b9053b44c8ca4c0bd
Edouard <edou4rd@gmail.com>
2015-03-30 11:48:43 +0200

    Blog Settings: e2e tests

---
+++ client/spec/blog_settings_spec.js
@@ -0,0 +1,65 @@
+var openUrl = require('./helpers/utils').open;
+var openBlog = require('./helpers/utils').openBlog;
+
+describe('Blog settings', function() {
+    'use strict';
+
+    var DEFAULT_LANGUAGE = 'en';
+    var NEW_LANGUAGE = 'fr';
+
+    beforeEach(openUrl('/#/liveblog'));
+
+    function openSettings() {
+        // click on the settings button
+        element(by.css('.settings-link')).click();
+    }
+
+    function expectSelectedLanguageIs(language) {
+        expect(element(by.model('settings.blogPreferences.language')).getAttribute('value')).toEqual(language);
+    }
+
+    function setLanguage(language) {
+        element(by.model('settings.blogPreferences.language')).sendKeys(language);
+    }
+
+    it('shows the default language selected', function() {
+        openBlog(0);
+        openSettings();
+        expectSelectedLanguageIs(DEFAULT_LANGUAGE);
+    });
+
+    it('save a new value for language', function() {
+        openBlog(0);
+        openSettings();
+        setLanguage(NEW_LANGUAGE);
+        // save
+        element(by.css('[ng-click="settings.save()"]')).click();
+        openSettings();
+        expectSelectedLanguageIs(NEW_LANGUAGE);
+    });
+
+    it('reset default language value', function() {
+        openBlog(0);
+        openSettings();
+        setLanguage(NEW_LANGUAGE);
+        // save a new value
+        element(by.css('[ng-click="settings.save()"]')).click();
+        openSettings();
+        expectSelectedLanguageIs(NEW_LANGUAGE);
+        // reset
+        element(by.css('[ng-click="settings.reset()"]')).click();
+        expectSelectedLanguageIs(DEFAULT_LANGUAGE);
+    });
+
+    it('cancel changed language', function() {
+        openBlog(0);
+        openSettings();
+        setLanguage(NEW_LANGUAGE);
+        // cancel
+        element(by.css('[ng-click="settings.close()"]')).click();
+        openSettings();
+        expectSelectedLanguageIs(DEFAULT_LANGUAGE);
+    });
+
+
+});
