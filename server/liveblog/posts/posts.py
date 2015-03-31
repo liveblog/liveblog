52ffa15ad30dc07a43d67f2b5e15eed5945e236c
Edouard <edou4rd@gmail.com>
2015-03-31 17:08:26 +0200

    posts.py

--- server/liveblog/posts/posts.py
+++ server/liveblog/posts/posts.py
@@ -65,8 +65,6 @@ class PostsService(ArchiveService):
         return docs
 
     def on_created(self, docs):
-        for doc in docs:
-            doc['type'] = 'composite'
         super().on_created(docs)
         push_notification('posts', created=1)
 
