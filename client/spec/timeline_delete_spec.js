f6f1b3ef0cdd31c21e3f7130203a656bcc4910da
vladnicoara <vlad.nicoara@sourcefabric.org>
2015-04-13 18:32:28 +0300

    LBSD-416 new timeline deletion e2e tests

--- client/spec/timeline_delete_spec.js
+++ client/spec/timeline_delete_spec.js
@@ -3,40 +3,24 @@ var openUrl = require('./helpers/utils').open,
     openBlog = require('./helpers/utils').openBlog;
 
 describe('timeline deletions', function() {
-    beforeEach(openUrl('/#/liveblog'));
-    it('can delete items on the timeline', function() {
-        openBlog(2);
-        element.all(by.repeater('post in posts')).then(function(posts) {
-            //this post should be single item post
-            posts[0].getText().then(function(txt) {
-                posts[0].element(by.css('.timeline-simple-item')).click();
-                posts[0].element(by.css('[ng-click="askRemoveItem(item, post)"]')).click();
-                confirmRemoval();
-                compareTexts(txt, 0);
-            });
-        });
-    });
+    beforeEach(function(done) {openUrl('/#/liveblog').then(done);});
     it('can delete posts on the timeline', function() {
-        openBlog(2);
+        openBlog(0);
         element.all(by.repeater('post in posts')).then(function(posts) {
-            //this post should be a multiple item one
-            posts[1].getText().then(function(txt) {
-                posts[1].element(by.css('.post-item__header')).click();
-                posts[1].element(by.css('[dropdown-toggle]')).click();
-                posts[1].element(by.css('[ng-click="askRemovePost(post)"]')).click();
-                confirmRemoval();
-                compareTexts(txt, 1);
-            });
+            posts[0].element(by.css('[ng-click="askRemovePost(post)"]')).click();
+            confirmRemoval();
+            compare(posts.length);
         });
     });
 });
-function compareTexts(oldText, position) {
+function compare(oldLength) {
     element.all(by.repeater('post in posts')).then(function(newPosts) {
-        expect(oldText).not.toEqual(newPosts[position].getText());
+        expect(oldLength).not.toEqual(newPosts.length);
     });
 }
-
 function confirmRemoval() {
-    browser.wait(element(by.css('[ng-click="ok()"')).isDisplayed);
-    element(by.css('[ng-click="ok()"')).click();
+    browser.wait(function() {
+        return element(by.css('.modal-footer.ng-scope')).isDisplayed();
+    });
+    element(by.css('button[ng-click="ok()"')).sendKeys(protractor.Key.ENTER);
 }
