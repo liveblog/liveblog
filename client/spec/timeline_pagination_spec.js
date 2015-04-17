3ebf94ac89505714f1a94d5de8090f8e52747eff
vladnicoara <vlad.nicoara@sourcefabric.org>
2015-04-17 17:39:26 +0300

    LBSD-408 Small changes to work with prepopulated data

--- client/spec/timeline_pagination_spec.js
+++ client/spec/timeline_pagination_spec.js
@@ -4,11 +4,11 @@ var openUrl = require('./helpers/utils').open,
 describe('timeline pagination', function() {
     beforeEach(function(done) {openUrl('/#/liveblog').then(done);});
     it('can scroll to last item and load more', function() {
-        openBlog(0);
+        openBlog(2);
         var postsNo = element.all(by.repeater('post in posts')).count(),
             lastPost = element.all(by.repeater('post in posts')).last();
         browser.driver.executeScript('arguments[0].scrollIntoView(true);', lastPost.getWebElement());
         browser.waitForAngular();
-        expect(by.repeater('post in posts')).toBeGreaterThan(postsNo);
+        expect(element.all(by.repeater('post in posts')).count()).toBeGreaterThan(postsNo);
     });
 });
