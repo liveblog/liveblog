130372b4c8e8251289a810f992cd93bbdc602f12
Edouard <edou4rd@gmail.com>
2015-04-22 11:04:43 +0200

    Unpublish a post from the timeline: don't update the post reference on saving operation to conserve its original update date in the posts list directive in order to retrieve updates from this date (if latest)
    LBSD-11

--- client/app/scripts/liveblog-edit/directives.js
+++ client/app/scripts/liveblog-edit/directives.js
@@ -192,6 +192,11 @@ define([
                                     });
                             },
                             unpublishPost: function(post) {
+                                // don't save the original post coming for the posts list, because it needs 
+                                // to conserve its original update date in the posts list directive
+                                // in order to retrieve updates from this date (if latest)
+                                post = angular.copy(post);
+                                // save the post as draft
                                 postsService.saveDraft(post.blog, post).then(function(post) {
                                     notify.pop();
                                     notify.info(gettext('Post saved as draft'));
