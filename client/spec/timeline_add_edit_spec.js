95047b379a4e0f427f2270f2eccee20f2ec79b65
Edouard <edou4rd@gmail.com>
2015-05-19 13:55:12 +0200

    spec/timeline_add_edit_spec.js factorized

--- client/spec/timeline_add_edit_spec.js
+++ client/spec/timeline_add_edit_spec.js
@@ -4,15 +4,21 @@ var openBlog = require('./helpers/utils').openBlog;
 var randomString = require('./helpers/utils').randomString;
 
 describe('timeline add to top and edit', function() {
-    beforeEach(function(done) {login().then(done);});
-    it('can add item to top of the timeline', function() {
-        openBlog(0);
+
+    function publishPost() {
         var randomText = randomString(10);
         //type the random text
         element(by.css('[class="st-required st-text-block"]')).sendKeys(randomText);
         //click the publish button
         element(by.css('[ng-click="publish()"]')).click();
-        browser.waitForAngular();
+        return randomText;
+    }
+
+    beforeEach(function(done) {login().then(done);});
+
+    it('can add item to top of the timeline', function() {
+        openBlog(0);
+        var randomText = publishPost();
         //go and check the timeline
         element(by.css('.column-timeline')).all(by.repeater('post in posts')).then(function(posts) {
             var textElement = posts[0].element(by.css('span[medium-editable]'));
@@ -22,6 +28,7 @@ describe('timeline add to top and edit', function() {
             });
         });
     });
+
     it('can edit an item on the timeline', function() {
 
         function getFirstPostText() {
@@ -44,6 +51,7 @@ describe('timeline add to top and edit', function() {
                 expect(getFirstPostText()).toBe(randomText);
             });
     });
+
     it('can edit an item on the timeline (quick edit mode)', function() {
         openBlog(2);
         var randomText = randomString(10);
@@ -81,6 +89,7 @@ describe('timeline add to top and edit', function() {
             });
         });
     });
+
     it('can unpublish your own post', function() {
 
         function getFirstPost(column) {
@@ -100,14 +109,6 @@ describe('timeline add to top and edit', function() {
                             expect(getFirstPost('draft-posts').element(by.css('.lb-post__list')).getText()).toBe(firstTimelinePostContent);
                         });
                 });
-        }
-
-        function publishPost() {
-            var randomText = randomString(10);
-            //type the random text
-            element(by.css('[class="st-required st-text-block"]')).sendKeys(randomText);
-            //click the publish button
-            element(by.css('[ng-click="publish()"]')).click();
         }
 
         openBlog(2);
