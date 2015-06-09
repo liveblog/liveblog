b4479549911df221975508b00e0af3551f3dcb33
Edouard <edou4rd@gmail.com>
2015-06-09 17:52:00 +0200

    Embed: First step to publish a blog on S3

--- server/manage.py
+++ server/manage.py
@@ -6,11 +6,17 @@ import superdesk
 from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
 from app import get_app
+import flask_s3
 
 app = get_app()
 manager = Manager(app)
-
 manager.add_command("assets", ManageAssets())
+
+
+@manager.command
+def publish():
+    flask_s3.create_all(app)
+
 
 if __name__ == '__main__':
     manager.run(superdesk.COMMANDS)
