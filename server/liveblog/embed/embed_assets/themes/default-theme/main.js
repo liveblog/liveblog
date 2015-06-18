047a9c945259ca33603a71d22c03c57172239ace
Edouard <edou4rd@gmail.com>
2015-06-18 17:21:41 +0200

    Default Theme: loading message

--- server/liveblog/embed/embed_assets/themes/default-theme/main.js
+++ server/liveblog/embed/embed_assets/themes/default-theme/main.js
@@ -4,6 +4,8 @@
     TimelineCtrl.$inject = ['$interval', 'PagesManager', 'blogs', 'config'];
     function TimelineCtrl($interval, PagesManager, blogsService, config) {
 
+        var POSTS_PER_PAGE = 20;
+        var DEFAULT_ORDER = 'editorial'; // newest_first, oldest_first or editorial
         var vm = this;
 
         function retrieveUpdate() {
@@ -13,20 +15,23 @@
         // define view model
         angular.extend(vm, {
             blog: config.blog,
-            showSplash: true,
             loading: true,
             finished: false,
-            order: 'editorial',
+            order: DEFAULT_ORDER,
             orderBy: function(order_by) {
                 vm.order = order_by;
-                vm.pagesManager.changeOrder(order_by);
+                vm.loading = true;
                 vm.finished = false;
+                vm.pagesManager.changeOrder(order_by).then(function() {
+                    vm.loading = false;
+                });
             },
             fetchNewPage: function() {
                 vm.loading = true;
                 return vm.pagesManager.fetchNewPage().then(function(data){
                     vm.loading = false;
                     vm.finished = data._meta.total <= data._meta.max_results;
+                    // TODO: notify updates
                 });
             },
             isAllowedToLoadMore: function() {
@@ -43,7 +48,7 @@
                         window.twttr.widgets.load(document.getElementById(element_id));
                     });
             },
-            pagesManager: new PagesManager(5, 'editorial')
+            pagesManager: new PagesManager(POSTS_PER_PAGE, DEFAULT_ORDER)
         });
         // retrieve first page
         vm.fetchNewPage()
