abf084d3f8393f821a3c4a5dc30ee1a952fa4df5
Edouard <edou4rd@gmail.com>
2015-06-29 15:05:45 +0200

    Hide the language setting from the general and blog settings

--- client/spec/general_settings_spec.js
+++ client/spec/general_settings_spec.js
@@ -3,9 +3,10 @@ var login = require('../app/scripts/bower_components/superdesk/client/spec/helpe
 describe('General settings', function() {
     'use strict';
 
-    var DEFAULT_LANGUAGE = 'english',
-        NEW_LANGUAGE = 'french',
-        DEFAULT_THEME = 'default-theme',
+    // FIXME: must be uncommented after release (LBSD-546)
+    // var DEFAULT_LANGUAGE = 'english',
+        // NEW_LANGUAGE = 'french',
+    var DEFAULT_THEME = 'default-theme',
         NEW_THEME = 'forest';
 
     beforeEach(function(done) {login().then(done);});
@@ -37,23 +38,25 @@ describe('General settings', function() {
         element(by.css('[ng-click="saveSettings()"]')).click();
     }
 
-    it('shows the default language', function() {
-        openLiveblogSettings();
-        expectSelected('liveblogSettings.language.value', DEFAULT_LANGUAGE);
-    });
+    // FIXME: must be uncommented after release (LBSD-546)
+    // it('shows the default language', function() {
+    //     openLiveblogSettings();
+    //     expectSelected('liveblogSettings.language.value', DEFAULT_LANGUAGE);
+    // });
 
     it('shows the default theme', function() {
         openLiveblogSettings();
         expectSelected('liveblogSettings.theme.value', DEFAULT_THEME);
     });
 
-    it('sets new language', function() {
-        openLiveblogSettings();
-        setSelected('liveblogSettings.language.value', NEW_LANGUAGE);
-        saveSettings();
-        openLiveblogSettings();
-        expectSelected('liveblogSettings.language.value', NEW_LANGUAGE);
-    });
+    // FIXME: must be uncommented after release (LBSD-546)
+    // it('sets new language', function() {
+    //     openLiveblogSettings();
+    //     setSelected('liveblogSettings.language.value', NEW_LANGUAGE);
+    //     saveSettings();
+    //     openLiveblogSettings();
+    //     expectSelected('liveblogSettings.language.value', NEW_LANGUAGE);
+    // });
 
     it('sets new theme', function() {
         openLiveblogSettings();
