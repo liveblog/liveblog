d14e89953c9077fe2d944bd61fa99c5d2cce127b
Edouard <edou4rd@gmail.com>
2015-09-01 15:06:35 +0200

    Themes: remove the automatic load of themes from local storage

--- server/app.py
+++ server/app.py
@@ -114,10 +114,6 @@ def get_app(config=None):
         prefix = app.api_prefix or None
         app.register_blueprint(blueprint, url_prefix=prefix)
 
-    #  load local themes
-    with app.app_context():
-        created, updated = superdesk.get_resource_service('themes').update_registered_theme_with_local_files(force=True)
-        print('%d themes registered' % (len(created) + len(updated)))
     # s3
     s3.init_app(app)
     # embed feature
