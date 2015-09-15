3f16a19717836e50dbb5f2bb4fc0fb64ef7d2f9a
Edouard <edou4rd@gmail.com>
2015-09-15 10:46:45 +0200

    autofocus on filter input field

--- client/app/scripts/liveblog-edit/directives.js
+++ client/app/scripts/liveblog-edit/directives.js
@@ -314,6 +314,16 @@ define([
                     });
                 }]
             };
+        }])
+        .directive('autofocus', ['$timeout', function($timeout) {
+            return {
+                restrict: 'A',
+                link : function($scope, $element) {
+                    $timeout(function() {
+                        $element[0].focus();
+                    });
+                }
+            };
         }]);
 });
 
