145691c47b3ddd97a856027a5f1a99b5aaed2673
vladnicoara <vlad.nicoara@sourcefabric.org>
2015-09-23 12:32:05 +0300

    LBSD-643 fix e2e tests

--- client/spec/themes_manager_spec.js
+++ client/spec/themes_manager_spec.js
@@ -12,8 +12,6 @@ describe('Themes Manager', function() {
         .themes
         .then(function(themes) {
             expect(themes.length).toBe(3);
-            // angular-base
-            themeManager.expectTheme(0, {number_of_blogs_expected: 0, is_default_theme:false, name: 'Angular Base'});
             // default-theme
             themeManager.expectTheme(1, {number_of_blogs_expected: 5, is_default_theme:true, name: 'Default Theme'});
             // open a thumbnail
