55f5e818f01d78319bcd8669e154e282e4b33cd3
Edouard <edou4rd@gmail.com>
2015-10-14 16:05:22 +0200

    default-theme renamed to classic, and angular-base to angular

--- client/spec/themes_manager_spec.js
+++ client/spec/themes_manager_spec.js
@@ -12,10 +12,10 @@ describe('Themes Manager', function() {
         .themes
         .then(function(themes) {
             expect(themes.length).toBe(3);
-            // angular-base
-            themeManager.expectTheme(0, {number_of_blogs_expected: 0, is_default_theme:false, name: 'Angular Base'});
-            // default-theme
-            themeManager.expectTheme(1, {number_of_blogs_expected: 5, is_default_theme:true, name: 'Default Theme'});
+            // angular
+            themeManager.expectTheme(0, {number_of_blogs_expected: 0, is_default_theme:false, name: 'Angular Based Theme'});
+            // classic
+            themeManager.expectTheme(1, {number_of_blogs_expected: 5, is_default_theme:true, name: 'Classic Theme'});
             // open a thumbnail
             themeManager.openPreview(1);
             // forest
@@ -29,8 +29,8 @@ describe('Themes Manager', function() {
         .then(function() {
             // forest
             themeManager.expectTheme(2, {number_of_blogs_expected: 0, is_default_theme:true, name: 'forest'});
-            // default-theme
-            themeManager.expectTheme(1, {number_of_blogs_expected: 5, is_default_theme:false, name: 'Default Theme'});
+            // classic
+            themeManager.expectTheme(1, {number_of_blogs_expected: 5, is_default_theme:false, name: 'Classic Theme'});
         });
     });
 
