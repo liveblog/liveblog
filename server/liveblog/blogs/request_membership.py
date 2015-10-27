619c12986f4ad685452b2ae9d9c72896210a1b31
Anca Farcas <anca.farcas@sourcefabric.org>
2015-10-27 17:22:52 +0200

    request for membership can be sent only one time

--- server/liveblog/blogs/request_membership.py
+++ server/liveblog/blogs/request_membership.py
@@ -20,6 +20,7 @@ from superdesk.notification import push_notification
 from superdesk.services import BaseService
 from superdesk.emails import send_email
 from flask import g
+from superdesk.errors import SuperdeskApiError
 
 logger = logging.getLogger('superdesk')
 
@@ -80,6 +81,11 @@ class MembershipService(BaseService):
     def on_create(self, docs):
         for doc in docs:
             doc['original_creator'] = str(get_user().get('_id'))
+            request_service = get_resource_service('request_membership').get(req=None, lookup={})
+            if request_service:
+                for r in request_service:
+                    if (str(get_user().get('_id')) == r['original_creator']):
+                        raise SuperdeskApiError.badRequestError(message='A request has already been sent')
         super().on_create(docs)
 
     def on_created(self, docs):
