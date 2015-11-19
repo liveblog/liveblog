18ff96822af025ed4c310c70912e99b2ce905482
Anca Farcas <anca.farcas@sourcefabric.org>
2015-11-19 15:05:58 +0200

    post a comment without being a logged-in-user

--- server/liveblog/client_modules/__init__.py
+++ server/liveblog/client_modules/__init__.py
@@ -1,7 +1,7 @@
 import superdesk
 from liveblog.client_modules.client_modules import ClientBlogsResource, ClientBlogsService,\
     ClientPostsService, ClientPostsResource, ClientUsersResource, ClientUsersService, ClientBlogPostsService,\
-    ClientBlogPostsResource, ClientCommentsService, ClientCommentsResource
+    ClientBlogPostsResource, ClientCommentsService, ClientCommentsResource, ClientItemsService, ClientItemsResource
 
 
 def init_app(app):
@@ -24,3 +24,7 @@ def init_app(app):
     endpoint_name = 'client_comments'
     service = ClientCommentsService(endpoint_name, backend=superdesk.get_backend())
     ClientCommentsResource(endpoint_name, app=app, service=service)
+
+    endpoint_name = 'client_items'
+    service = ClientItemsService(endpoint_name, backend=superdesk.get_backend())
+    ClientItemsResource(endpoint_name, app=app, service=service)
