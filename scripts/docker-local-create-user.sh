f7d8061d964301b41fbb2d4cc6640fdc8f3cca4e
actionless <actionless.loveless@gmail.com>
2015-12-01 09:54:24 +0100

    chore(docker): merge client and server containers; add more docker-compose profiles

--- scripts/docker-local-create-user.sh
+++ scripts/docker-local-create-user.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
 SCRIPT_DIR=$(readlink -e $(dirname "$0"))
-cd $SCRIPT_DIR/../docker &&
-docker-compose run backend ./scripts/fig_wrapper.sh python3 manage.py users:create -u admin -p admin -e 'admin@example.com' --admin=true
+(test -d $SCRIPT_DIR/env || virtualenv -p python2 $SCRIPT_DIR/env ) &&
+. $SCRIPT_DIR/env/bin/activate
+set -ue
+pip install -r $SCRIPT_DIR/../docker/requirements.txt
+cd $SCRIPT_DIR/../docker
+
+
+docker-compose -p lbdemo -f ./docker-compose-prebuilt.yml run superdesk ./scripts/fig_wrapper.sh bash -c "\
+  python3 manage.py app:initialize_data ;\
+  echo '+++ sample data was prepopulated' ;\
+  python3 manage.py users:create -u admin -p admin -e 'admin@example.com' --admin=true ;\
+  echo '+++ new user has been created' ;\
+  python3 manage.py register_local_themes ;\
+  echo '+++ liveblog: local themes were registered'"
