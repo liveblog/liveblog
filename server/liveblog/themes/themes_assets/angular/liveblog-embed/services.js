50897e5172e3dfbdae14aa1f6f80057b0df54f64
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2016-03-29 02:45:56 +0300

    feature: added asset provider.

--- server/liveblog/themes/themes_assets/angular/liveblog-embed/services.js
+++ server/liveblog/themes/themes_assets/angular/liveblog-embed/services.js
@@ -10,5 +10,24 @@
                 text.replace(absoluteProtocol, '//')
                 return text.replace(absoluteProtocol, '//')
             };
+        }])
+        .provider('asset', [ '$injector', function ($injector) {
+            this.templateUrl = function(path) {
+                var config = $injector.get('config'),
+                    ret = path;
+                if (!/^(https?:\/\/|\/\/)/.test(path)) {
+                    ret = config.assets_root +'/' + ret;
+                }
+                ret = ret.replace(/[^\/]+\/+\.\.\//g, '')
+                         .replace(/\.\//g, '')
+                         .replace(/(\w)\/\/(\w)/g, '$1/$2');
+                return ret;
+            };
+
+            this.imageUrl = this.templateUrl;
+
+            this.$get = function() {
+                return this;
+            };
         }]);
 })(angular);
