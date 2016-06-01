2f47397e30e31d24fcb7469f45ae80532f93854c
psnewslab <paul.solbach@dpa-info.com>
2016-06-01 18:58:20 +0200

    content height adjust by parent iframe height via postmessage API

---
+++ js/iframe.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/*
+  Resize parent iframe from postmessage
+  received via iframe onload event containing the iframe height
+*/
+
+function receiveMessage(e) {
+  console.log("processing message.")
+
+  var d = e.data; // height in px  
+  var m = e.data.split(':'); // should be type:data
+  var h = m[1]; // height 
+  
+  if (!m.length===2) return; // wrong event
+  if (parseInt(h, 10) && m[0] === "resize") { // if integer and msg type resize
+    resizeIframeHeight(h) // set timeline height to parent iframe size
+  }
+}
+
+function resizeIframeHeight(h) { // T
+  document.getElementsByClassName("lb-timeline")[0].style.height = (h - 250) + "px";
+}
+
+module.exports = function() {
+  window.addEventListener("message", receiveMessage, false);
+  //var parentHeight = parent.document.body.clientHeight
+}
--- js/onload.js
+++ js/onload.js
@@ -1,14 +1,17 @@
 'use strict';
 var angular = require("angular")
+  , registerParentHandler = require('./iframe')
   , _ = require('./lodash-custom')
   , _log = require("./log");
 
 module.exports = function($rootScope, $window, $timeout, resizeIframes, config) {
   var timeline = document.getElementsByClassName("lb-timeline")[0];
   timeline.classList.remove("mod--hide"); // Fade in timeline after all js has loaded
-
   $rootScope._log = _log(config); // Logging helper
+
   $window.onload = function() {
+    $rootScope._log.debug("ng-lb", "started");
+    registerParentHandler(); // Adjust body height to parent iframe
     $timeout(resizeIframes, 1000); // Hacky, hacky
     angular.element($window).bind('resize', 
       _.debounce(resizeIframes, 1000));
