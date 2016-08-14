396dbf5529dfd32fe2041239fe453f2e6e5c13ee
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2016-08-14 22:50:18 +0300

    Squashed 'server/liveblog/themes/themes_assets/aachener/' content from commit 2ca43c4
    git-subtree-dir: server/liveblog/themes/themes_assets/aachener
    git-subtree-split: 2ca43c4e646e15f0d314b5931f070bfe303a2b27

---
+++ main.js
@@ -0,0 +1,180 @@
+(function(angular) {
+    'use strict';
+
+    TimelineCtrl.$inject = ['$interval', 'PagesManager', 'blogs', 'config', '$anchorScroll', '$timeout', 'Permalink', 'transformBlog', 'gettext'];
+    function TimelineCtrl($interval, PagesManager, blogsService, config, $anchorScroll, $timeout, Permalink, transformBlog, gettext) {
+
+        var POSTS_PER_PAGE = config.settings.postsPerPage;
+        var STICKY_POSTS_PER_PAGE = 100;
+        var PERMALINK_DELIMITER = config.settings.permalinkDelimiter || '?';
+        var DEFAULT_ORDER = config.settings.postOrder; // newest_first, oldest_first or editorial
+        var UPDATE_MANUALLY = config.settings.loadNewPostsManually;
+        var UPDATE_EVERY = 10*1000; // retrieve update interval in millisecond
+        var vm = this;
+        var pagesManager = new PagesManager(POSTS_PER_PAGE, DEFAULT_ORDER, false),
+            permalink = new Permalink(pagesManager, PERMALINK_DELIMITER);
+
+        var stickyPagesManager = new PagesManager(STICKY_POSTS_PER_PAGE, DEFAULT_ORDER, true),
+            stickyPermalink = new Permalink(stickyPagesManager, PERMALINK_DELIMITER);
+
+        function retrieveUpdate() {
+            return vm.pagesManager.retrieveUpdate(!UPDATE_MANUALLY).then(function(data) {
+                vm.newPosts = data._items;
+            });
+        }
+
+        function retrieveStickyUpdate() {
+            return vm.stickyPagesManager.retrieveUpdate(!UPDATE_MANUALLY).then(function(data) {
+                vm.newStickyPosts = data._items;
+            });
+        }
+
+        function retrieveBlogSettings() {
+            blogsService.get({}, function(blog) {
+                angular.extend(vm.blog, blog);
+            });
+        }
+
+        // define view model
+        angular.extend(vm, {
+            templateDir: config.assets_root,
+            blog: transformBlog(config.blog),
+            loading: true,
+            finished: false,
+            highlightsOnly: false,
+            settings: config.settings,
+            newPosts: [],
+            newStickyPosts: [],
+            sortOptions: [{
+                name: gettext('Editorial'),
+                order: 'editorial'
+            }, {
+                name: gettext('Newest first'),
+                order: 'newest_first'
+            }, {
+                name: gettext('Oldest first'),
+                order: 'oldest_first'
+            }],
+            orderBy: function(order_by) {
+                vm.loading = true;
+                vm.finished = false;
+                vm.pagesManager.changeOrder(order_by).then(function(data) {
+                    vm.loading = false;
+                    vm.finished = data._meta.total <= data._meta.max_results * data._meta.page;
+                });
+            },
+            fetchNewPage: function() {
+                vm.loading = true;
+                return vm.pagesManager.fetchNewPage().then(function(data){
+                    vm.loading = false;
+                    vm.finished = data._meta.total <= data._meta.max_results * data._meta.page;
+                    // TODO: notify updates
+                });
+            },
+            permalinkScroll: function() {
+                vm.loading = true;
+                vm.permalink.loadPost().then(function(id){
+                    $anchorScroll(id);
+                    vm.loading = false;
+                }, function(){
+                    vm.loading = false;
+                });
+            },
+            isAllowedToLoadMore: function() {
+                return !vm.loading && !vm.finished;
+            },
+            applyUpdates: function() {
+                pagesManager.applyUpdates(vm.newPosts);
+                vm.newPosts = [];
+                stickyPagesManager.applyUpdates(vm.newStickyPosts);
+                vm.newStickyPosts = [];
+            },
+            toggleHighlighsOnly: function() {
+                vm.highlightsOnly = !vm.highlightsOnly;
+                vm.loading = true;
+                vm.finished = false;
+                stickyPagesManager.changeHighlight(vm.highlightsOnly);
+                pagesManager.changeHighlight(vm.highlightsOnly).then(function(data) {
+                    vm.loading = false;
+                    vm.finished = data._meta.total <= data._meta.max_results * data._meta.page;
+                });
+                if (vm.highlightsOnly) {
+                    stickyPagesManager.hideSticky = false;
+                }
+            },
+            pagesManager: pagesManager,
+            permalink: permalink,
+            stickyPagesManager: stickyPagesManager,
+            stickyPermalink: stickyPermalink
+        });
+        //get the first sticky page only once
+        vm.stickyPagesManager.fetchNewPage();
+        // retrieve regular first page
+        vm.fetchNewPage()
+        // retrieve updates periodically
+        .then(function() {
+            vm.permalinkScroll();
+            $interval(retrieveUpdate, UPDATE_EVERY);
+            $interval(retrieveStickyUpdate, UPDATE_EVERY);
+            $interval(retrieveBlogSettings, 3 * UPDATE_EVERY);
+            // listen events from parent
+            var fetchNewPageDebounced = _.debounce(vm.fetchNewPage, 1000);
+            function receiveMessage(event) {
+                if (event.data === 'loadMore') {
+                    fetchNewPageDebounced();
+                }
+            }
+            window.addEventListener('message', receiveMessage, false);
+        });
+    }
+
+    angular.module('theme', ['liveblog-embed', 'ngAnimate', 'infinite-scroll', 'gettext'])
+        .run(['gettextCatalog', 'config', function (gettextCatalog, config) {
+            gettextCatalog.setCurrentLanguage(config.settings.language);
+        }])
+        .run(['$rootScope', function($rootScope){
+            angular.element(document).on("click", function(e) {
+                $rootScope.$broadcast("documentClicked", angular.element(e.target));
+            });
+        }])
+        .controller('TimelineCtrl', TimelineCtrl)
+        .directive('lbItem', ['asset', function(asset) {
+            return {
+                restrict: 'AE',
+                scope: {
+                    ident: '=',
+                    item: '='
+                },
+                templateUrl: asset.templateUrl('views/item.html'),
+            }
+        }])
+        .directive('lbAuthor', ['asset', function(asset) {
+            return {
+                restrict: 'AE',
+                scope: {
+                    item: '=',
+                    timeline: '='
+                },
+                templateUrl: asset.templateUrl('views/author.html'),
+            }
+        }])
+        //alternative for a 2nd format (as an example for when time should be shown separately from the date)
+        .filter('prettifyIsoDateAlt', [ 'config', function(config) {
+            return function(input) {
+                return moment(input).format(config.settings.datetimeFormatAlt? config.settings.datetimeFormatAlt: 'HH:mm');
+            };
+        }])
+        .directive('lbPosts', ['asset', function(asset) {
+            return {
+                restrict: 'E',
+                scope: {
+                    posts: '=',
+                    timeline: '='
+                },
+                templateUrl: asset.templateUrl('views/posts.html'),
+            }
+        }]);
+
+    angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 1000);
+
+})(angular);
