e67614136d4bdced06e8d957a98c82498f504535
Loïc Nogues <nogues.loic@gmail.com>
2016-10-07 16:09:45 +0200

    honcho pointing to procfile dev

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,5 +3,5 @@ sudo service redis-server restart
 
 sleep 5
 
-cd /opt/liveblog/server && honcho start &
+cd /opt/liveblog/server && honcho -f ../docker/Procfile-dev start &
 cd /opt/liveblog/client && grunt --force server --server='http://localhost:5000/api' --ws='ws://localhost:5100'
