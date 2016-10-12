f7eb32ded4437d81c1f1f59f9a51b722cb7da635
Loïc Nogues <nogues.loic@gmail.com>
2016-10-12 16:31:11 +0200

    Added the missing socket url option

--- client/config.sample.js
+++ client/config.sample.js
@@ -1,12 +1,16 @@
 export default {
     server: {
-        url: 'http://localhost:5000/api'
+        url: 'http://localhost:5000/api',
+        ws: 'http://localhost:5100'
+    },
+    editor: {
+        disableEditorToolbar: false
     },
     analytics: {
         piwik: '',
         ga: ''
     },
     embedly: {
-        key: 'type your key here'
+        key: '1163b640a11444e39ec77aa3735720e6'
     }
 };
