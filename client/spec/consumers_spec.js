1bc93ee37fa36b17a621465f0951e19b50473398
Loïc Nogues <nogues.loic@gmail.com>
2016-10-18 15:48:39 +0200

    Remove comments

--- client/spec/consumers_spec.js
+++ client/spec/consumers_spec.js
@@ -1,16 +1,15 @@
 var login = require('../app/scripts/bower_components/superdesk/client/spec/helpers/utils').login,
-    logout = require('./helpers/utils').logout,
     consumersManagement = require('./helpers/pages').consumersManagement;
 
-fdescribe('Consumers', function() {
+describe('Consumers', function() {
+    'use strict';
+
     beforeEach(function(done) {login().then(done);});
 
     describe('list', function() {
         it('can open consumers managements and list the consumers', function() {
             consumersManagement
-                .openConsumersManagement()
-                .then(function() {
-                });
+                .openConsumersManagement();
         });
     });
 });
