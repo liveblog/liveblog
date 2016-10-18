297eac4d5a98117b2e42380b3e6f63a61321589b
Loïc Nogues <nogues.loic@gmail.com>
2016-10-18 12:04:48 +0200

    Update a consumer

--- client/app/scripts/liveblog-syndication/directives/consumer-edit.js
+++ client/app/scripts/liveblog-syndication/directives/consumer-edit.js
@@ -1,22 +1,45 @@
 liveblogSyndication
-    .directive('lbConsumerEdit', ['api', 'notify', function(api, notify) {
+    .directive('lbConsumerEdit', ['api', 'notify', 'lodash', function(api, notify, _) {
         return {
             templateUrl: 'scripts/liveblog-syndication/views/edit-form.html',
             scope: {
-                //consumer: '=',
+                consumer: '=',
                 onsave: '&',
                 oncancel: '&',
                 onupdate: '&'
             },
             link: function(scope, elem) {
-                console.log('current consumer', scope.consumer);
+                //var origConsumer = _.clone(scope.consumer);
+                var origConsumer;
+
+                console.log('form', scope.consumerForm);
+                angular.copy(scope.consumer, origConsumer);
 
                 scope.save = function() {
-                    api.consumers.save(scope.consumer).then(function(result) {
+                    if (angular.equals(origConsumer, scope.consumer))
+                        return;
+
+                    console.log('before save', origConsumer, scope.consumer, _.difference(origConsumer, scope.consumer));
+                    console.log('form', scope.consumerForm.$pristine);
+
+                    var data = {};
+                    var apiQuery;
+
+                    if (!scope.consumerForm.name.$pristine)
+                        data.name = scope.consumer.name;
+
+                    if (origConsumer)
+                        apiQuery = api.save('consumers', origConsumer, data);
+                    else
+                        apiQuery = api.consumers.save(data);
+
+                    apiQuery.then(function(result) {
+                        console.log('result', result);
                         notify.pop();
                         notify.success(gettext('consumer saved.'));
 
-                        scope.onsave({ consumer: result });
+                        if (!origConsumer)
+                            scope.onsave({ consumer: result });
                     });
                 };
             }
