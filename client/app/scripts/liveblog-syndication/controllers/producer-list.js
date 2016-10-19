17420ab91d572e3dcadc59bf8c78985fa29ca12c
Loïc Nogues <nogues.loic@gmail.com>
2016-10-19 14:08:19 +0200

    Producer management

--- client/app/scripts/liveblog-syndication/controllers/producer-list.js
+++ client/app/scripts/liveblog-syndication/controllers/producer-list.js
@@ -2,9 +2,22 @@ liveblogSyndication
     .controller('ProducerController', ['$scope', 'api', function($scope, api) {
         api.producers.query().then(function(producers) {
             $scope.producers = producers;
+        })
+        .catch(function(err) {
+            notify.pop();
+            notify.error(gettext('Fatal error!'));
         });
 
         $scope.createProducer = function() {
             $scope.selected = { producer: { name: ''}};
+        };
+
+        $scope.render = function(newProducer) {
+            $scope.producers._items.unshift(newProducer);
+            $scope.selected = {};
+        };
+
+        $scope.closePreview = function() {
+            $scope.selected = {};
         };
     }]);
