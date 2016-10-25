bd2891fadcaff0e0a148a2450fa038845d8c1044
daniel.read <daniel.read@sourcefabric.org>
2016-10-25 15:11:08 +0200

    wip

---
+++ client/app/scripts/liveblog-syndication/directives/contacts-edit.js
@@ -0,0 +1,15 @@
+liveblogSyndication
+    .directive('lbContactEdit', function() {
+        return {
+            templateUrl: 'scripts/liveblog-syndication/views/contacts-edit-form.html',
+            scope: {
+                contacts: '='
+            },
+            link: function(scope, elem, attrs) {
+                if (!scope.contacts) {
+                    scope.contacts = [{first_name: ''}];
+                }
+            }
+        }
+
+    });
