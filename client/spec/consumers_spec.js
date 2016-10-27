48a3b5ff40323a5aac9a0c1c19e14613e387467a
Loïc Nogues <nogues.loic@gmail.com>
2016-10-27 18:00:30 +0200

    Test consumer creation

--- client/spec/consumers_spec.js
+++ client/spec/consumers_spec.js
@@ -1,6 +1,21 @@
 var login = require('../app/scripts/bower_components/superdesk/client/spec/helpers/utils').login,
     consumersManagement = require('./helpers/pages').consumersManagement;
 
+function assertToastMsg(type, msg) {
+    var cssSelector = '.notification-holder .alert-' + type,
+        toast = $(cssSelector);
+
+    browser.sleep(500);
+    browser.ignoreSynchronization = true;
+    expect(toast.getText()).toContain(msg);
+    browser.sleep(500);
+    browser.ignoreSynchronization = false;
+}
+
+const consumer = {
+    name: 'Massey Fergusson'
+};
+
 describe('Consumers', function() {
     'use strict';
 
@@ -8,8 +23,35 @@ describe('Consumers', function() {
 
     describe('list', function() {
         it('can open consumers managements and list the consumers', function() {
-            consumersManagement
-                .openConsumersManagement();
+            consumersManagement.openConsumersManagement();
         });
+
+        fit('can create a new consumer', function() {
+            consumersManagement.openConsumersManagement();
+
+            element(by.css('button.navbtn.sd-create-btn'))
+                .click()
+                .then(function() {
+                    return element(by.css('input#name')).isDisplayed();
+                })
+                .then(function() {
+                    return element(by.css('input#name')).sendKeys(consumer.name);
+                })
+                .then(function() {
+                    return element(by.css('#save-edit-btn')).click();
+                })
+                .then(function() {
+                    return assertToastMsg('success', 'consumer saved.');
+                })
+                .then(function() {
+                    var firstRowName = element(by.css('ul.table-body div.row-wrapper div.name'));
+                    expect(firstRowName.getText()).toEqual(consumer.name);
+                    return;
+                });
+        });
+
+        //it('can update a consumer', function() {});
+
+        //it('can delete a consumer', function() {});
     });
 });
