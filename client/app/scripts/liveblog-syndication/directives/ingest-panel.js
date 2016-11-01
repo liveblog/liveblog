933bf2141f71444078f18f284994373f20f3e734
Loïc Nogues <nogues.loic@gmail.com>
2016-11-01 12:36:19 +0100

    Attach blog modal

--- client/app/scripts/liveblog-syndication/directives/ingest-panel.js
+++ client/app/scripts/liveblog-syndication/directives/ingest-panel.js
@@ -1,6 +1,27 @@
 liveblogSyndication
-    .directive('lbIngestPanel', function() {
+    .directive('lbIngestPanel', ['api', function(api) {
         return {
             templateUrl: 'scripts/liveblog-syndication/views/ingest-panel.html',
+            link: function(scope) {
+                scope.openSyndBlogsList = function() {
+                    scope.syndBlogsListModalActive = true;
+                }
+
+                scope.cancel = function() {
+                    scope.syndBlogsListModalActive = false;
+                }
+
+                api.producers.query().then((producers) => {
+                    scope.producers = producers;
+                });
+
+                scope.selectProducer = function(producerId) {
+                    api.get('/producers/' + producerId + '/blogs')
+                        .then(function(blogs) {
+                            console.log('blogs', blogs);
+                            scope.blogs = blogs;
+                        });
+                };
+            }
         };
-    });
+    }]);
