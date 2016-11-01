fc9f17b00539e1ccc26d54c53dca82c9f8435211
Loïc Nogues <nogues.loic@gmail.com>
2016-11-01 17:24:38 +0100

    Send syndication query

--- client/app/scripts/liveblog-syndication/directives/attach-syndicated-blogs-modal.js
+++ client/app/scripts/liveblog-syndication/directives/attach-syndicated-blogs-modal.js
@@ -1,42 +1,68 @@
 liveblogSyndication
-    .directive('lbAttachSyndicatedBlogsModal', ['api', function(api) {
-        return {
-            templateUrl: 'scripts/liveblog-syndication/views/attach-syndicated-blogs-modal.html',
-            scope: {
-                modalActive: '='
-            },
-            link: function(scope) {
-                scope.blogsToAttach = [];
+    .directive('lbAttachSyndicatedBlogsModal',
+        ['api', 'config', '$http', '$routeParams', function(api, config, $http, $routeParams) {
+            return {
+                templateUrl: 'scripts/liveblog-syndication/views/attach-syndicated-blogs-modal.html',
+                scope: {
+                    modalActive: '='
+                },
+                link: function(scope) {
+                    scope.blogsToAttach = [];
 
-                scope.cancel = function() {
-                    scope.syndBlogsListModalActive = false;
-                }
+                    var consumerBlogId = $routeParams._id;
 
-                api.producers.query().then((producers) => {
-                    scope.producers = producers;
-                });
-
-                scope.selectProducer = function(producerId) {
-                    scope.producers._items.forEach(function(producer) {
-                        if (producer._id == producerId)
-                            scope.currentProducer = producer;
+                    api.producers.query().then((producers) => {
+                        scope.producers = producers;
                     });
 
-                    api.get('/producers/' + producerId + '/blogs')
-                        .then(function(blogs) {
-                            console.log('blogs', blogs);
-                            scope.blogs = blogs;
+                    scope.cancel = function() {
+                        scope.syndBlogsListModalActive = false;
+                    }
+
+                    scope.selectProducer = function(producerId) {
+                        scope.producers._items.forEach(function(producer) {
+                            if (producer._id == producerId)
+                                scope.currentProducer = producer;
                         });
-                };
 
-                scope.check = function(blog) {
-                    blog.checked = (blog.hasOwnProperty('checked')) ? !blog.checked : true;
+                        api.get('/producers/' + producerId + '/blogs')
+                            .then(function(blogs) {
+                                console.log('blogs', blogs);
+                                scope.blogs = blogs;
+                            });
+                    };
 
-                    if (blog.checked && scope.blogsToAttach.indexOf(blog._id) == -1)
-                        scope.blogsToAttach.push(blog._id);
-                    else if (!blog.checked && scope.blogsToAttach.indexOf(blog._id) != -1)
-                        scope.blogsToAttach.splice(scope.blogsToAttach.indexOf(blog._id), 1);
-                };
-            }
-        };
-    }]);
+                    scope.check = function(blog) {
+                        blog.checked = (blog.hasOwnProperty('checked')) ? !blog.checked : true;
+
+                        if (blog.checked && scope.blogsToAttach.indexOf(blog._id) == -1)
+                            scope.blogsToAttach.push(blog._id);
+                        else if (!blog.checked && scope.blogsToAttach.indexOf(blog._id) != -1)
+                            scope.blogsToAttach.splice(scope.blogsToAttach.indexOf(blog._id), 1);
+                    };
+
+                    var syndicate = function(blog) {
+                        var uri = config.server.url + 
+                            '/producers/' + scope.currentProducer._id + 
+                            '/syndicate/' + blog._id;
+
+                        // I'm using angular default $http service because I couldn't manage
+                        // to have the superdesk api service to do what I want.
+                        $http.post(uri, { consumer_blog_id: consumerBlogId })
+                            .then(function(response) {
+                                console.log('response', response);
+                            })
+                            .catch(function(err) {
+                                console.log('err', err);
+                            });
+                    };
+
+                    scope.attach = function() {
+                        scope.blogs._items.forEach(function(blog) {
+                            if (scope.blogsToAttach.indexOf(blog._id) != -1)
+                                syndicate(blog)
+                        });
+                    }
+                }
+            };
+        }]);
