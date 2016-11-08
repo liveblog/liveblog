9939c7511ea81b50db1007efb7b7ecc457a01095
Loïc Nogues <nogues.loic@gmail.com>
2016-11-08 14:53:14 +0100

    Bootstrapping

---
+++ client/app/scripts/liveblog-syndication/directives/incoming-syndication.js
@@ -0,0 +1,8 @@
+liveblogSyndication
+    .directive('lbIncomingSyndication', function() {
+        return {
+            templateUrl: 'scripts/liveblog-syndication/views/incoming-syndication.html',
+            link: function(scope) {
+            }
+        };
+    });
