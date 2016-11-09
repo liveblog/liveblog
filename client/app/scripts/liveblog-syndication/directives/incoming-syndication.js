c4f3c4729ec2cd6d52164f63895e9c2f2c8f21b0
Loïc Nogues <nogues.loic@gmail.com>
2016-11-09 17:42:57 +0100

    Basic flux structure for incoming syndication

--- client/app/scripts/liveblog-syndication/directives/incoming-syndication.js
+++ client/app/scripts/liveblog-syndication/directives/incoming-syndication.js
@@ -1,20 +1,20 @@
 liveblogSyndication
-    .directive('lbIncomingSyndication', ['PagesManager', '$routeParams', 
-        function(PagesManager, $routeParams) {
+    .directive('lbIncomingSyndication',
+        ['$routeParams', 'IncomingSyndicationActions', 'IncomingSyndicationReducers', 'Store',
+        function($routeParams, IncomingSyndicationActions, IncomingSyndicationReducers, Store) {
             return {
                 templateUrl: 'scripts/liveblog-syndication/views/incoming-syndication.html',
                 link: function(scope) {
-                    var blogId = $routeParams._id,
-                        status = 'draft',
-                        pagesManager = new PagesManager(blogId, status, 10, 'editorial');
-
-                    scope.posts = {};
-                    console.log('pages manager');
-
-                    pagesManager.fetchNewPage().then(function(data) {
-                        console.log('data', data);
-                        scope.posts = data;
+                    scope.store = new Store(IncomingSyndicationReducers, {
+                        posts: {}
                     });
+
+                    scope.store.connect(function(state) {
+                        scope.posts = state.posts;
+                    });
+
+                    IncomingSyndicationActions
+                        .getPosts($routeParams._id, 'draft');
                 }
             };
         }]);
