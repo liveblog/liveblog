601aaae4a1c25edaea94c47d63769b09568e8626
Loïc Nogues <nogues.loic@gmail.com>
2016-11-09 17:06:54 +0100

    Displaying draft as placeholder data

--- client/app/scripts/liveblog-syndication/directives/incoming-syndication.js
+++ client/app/scripts/liveblog-syndication/directives/incoming-syndication.js
@@ -1,8 +1,20 @@
 liveblogSyndication
-    .directive('lbIncomingSyndication', function() {
-        return {
-            templateUrl: 'scripts/liveblog-syndication/views/incoming-syndication.html',
-            link: function(scope) {
-            }
-        };
-    });
+    .directive('lbIncomingSyndication', ['PagesManager', '$routeParams', 
+        function(PagesManager, $routeParams) {
+            return {
+                templateUrl: 'scripts/liveblog-syndication/views/incoming-syndication.html',
+                link: function(scope) {
+                    var blogId = $routeParams._id,
+                        status = 'draft',
+                        pagesManager = new PagesManager(blogId, status, 10, 'editorial');
+
+                    scope.posts = {};
+                    console.log('pages manager');
+
+                    pagesManager.fetchNewPage().then(function(data) {
+                        console.log('data', data);
+                        scope.posts = data;
+                    });
+                }
+            };
+        }]);
