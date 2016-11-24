3b0570a6f1f4305ed5bdd8a6584e5fcd4b7757fa
Loïc Nogues <nogues.loic@gmail.com>
2016-11-24 16:52:17 +0100

    wip

--- client/spec/syndication_spec.js
+++ client/spec/syndication_spec.js
@@ -22,12 +22,56 @@ describe('Syndication', function() {
     beforeEach(function(done) {login().then(done);});
 
     describe('Ingest Panel', function() {
-        it('should display stuff', function() {
+        it('should list available syndications', function() {
             navigateToIngestPanel()
                 .then(function() {
                     return element.all(by.repeater('blog in locallySyndicatedItems'))
                         .isDisplayed();
+                })
+                .then(function() {
+                    return element.all(by.repeater('blog in locallySyndicatedItems'))
+                        .count();
+                })
+                .then(function(count) {
+                    expect(count).toEqual(1);
                 });
         });
+
+        //it('should delete a syndication', function() {
+        //    navigateToIngestPanel()
+        //        .then(function() {
+        //            return element.all(by.repeater('blog in locallySyndicatedItems'))
+        //                .isDisplayed();
+        //        })
+        //        .then(function() {
+        //            return element.all(by.repeater('blog in locallySyndicatedItems'))
+        //                .get(0)
+        //                .element(by.css('button[ng-click="toggleDropdown($event, blog)"]'))
+        //                .click();
+        //        })
+        //        .then(function() {
+        //            return element.all(by.repeater('blog in locallySyndicatedItems'))
+        //                .get(0)
+        //                .element(by.css('button[ng-click="destroy($event, blog)"]'))
+        //                .isDisplayed();
+        //        })
+        //        .then(function() {
+        //            return element.all(by.repeater('blog in locallySyndicatedItems'))
+        //                .get(0)
+        //                .element(by.css('button[ng-click="destroy($event, blog)"]'))
+        //                .click();
+        //        })
+        //        //.then(function() {
+        //        //    return browser.sleep(500);
+        //        //})
+        //        .then(function() {
+        //            browser.sleep(500);
+        //            return element.all(by.repeater('blog in locallySyndicatedItems'))
+        //                .count();
+        //        })
+        //        .then(function(count) {
+        //            expect(count).toEqual(0);
+        //        });
+        //});
     });
 });
