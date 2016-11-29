c0c17843f9626dfbb7a08f865ad8acb972a00824
Loïc Nogues <nogues.loic@gmail.com>
2016-11-29 18:09:41 +0100

    Login with node

--- client/spec/helpers/webhook.js
+++ client/spec/helpers/webhook.js
@@ -1,8 +1,6 @@
 'use strict';
 
-//var querystring = require('querystring');
-var http = require('http');
-//var webhookJson = require('./webhook.json');
+const request = require('request');
 
 var Webhook = function(params) {
     this.serverUrl = params.baseBackendUrl;
@@ -19,78 +17,97 @@ var Webhook = function(params) {
 
 Webhook.prototype.login = function() {
     return this.request({ 
-        path: '/api/auth', 
+        path: 'auth', 
         method: 'POST',
         data: {
-            username: this.username,
-            password: this.password
+            "username": this.username,
+            "password": this.password
         }
     });
 };
 
 Webhook.prototype.request = function(params) {
-    var postData;
-
     var options = {
-        host: this.host,
-        port: this.port,
-        path: params.path,
+        url: this.serverUrl + params.path,
         method: (params.hasOwnProperty('method')) ? params.method : 'GET',
+        json: params.data,
         headers: {
-            'Content-Type': 'application/json;charset=UTF-8'
-            //'Accept': 'application/json, text/plain, */*'
-            //'Accept-Encoding': 'gzip, deflate',
-            //'Accept-Language': 'en-US,en;q=0.8',
-            //'Host': 'undefined.local:5000',
-            //'Origin': 'http://localhost:9000',
-            //'Referer': 'http://localhost:9000/'
+            "Content-Type": "application/json;charset=UTF-8"
         }
     };
 
-    if (options.method === 'POST') {
-        postData = JSON.stringify(params.data);
-        //options.headers['Content-Length'] = Buffer.byteLength(postData);
-        //console.log('post', postData);
-    }
-
-    console.log('options', options);
-
+    //console.log('OPTIONS', options);
     return new Promise(function(resolve, reject) {
-        var req = http.request(options, function(response) {
-            //console.log('perform query', response);
-            var bodyString = '';
-            response.setEncoding('utf8');
-
-            response.on('data', function(chunk) {
-                console.log('receiving data...');
-                bodyString += chunk;
-            });
-
-            response.on('end', function() {
-                console.log('end', bodyString);
-                resolve(bodyString);
-            });
+        request(options, function(err, response, body) {
+            console.log(err, body);
+            if (err)
+                reject(err);
+            else
+                resolve(body);
         });
-
-        console.log('promise');
-
-        req.on('error', function(err) {
-            console.log('ERR', err);
-            reject(err);
-        });
-
-        req.on('timeout', function() {
-            console.log('timeout');
-        });
-
-        if (options.method === 'POST') {
-            //req.write(postData);
-            console.log('post', postData);
-            req.write(postData);
-        }
-
-        req.end();
     });
+    //var postData;
+
+    //var options = {
+    //    host: this.host,
+    //    port: this.port,
+    //    path: params.path,
+    //    method: (params.hasOwnProperty('method')) ? params.method : 'GET',
+    //    headers: {
+    //        'Content-Type': 'application/json;charset=UTF-8'
+    //        //'Accept': 'application/json, text/plain, */*'
+    //        //'Accept-Encoding': 'gzip, deflate',
+    //        //'Accept-Language': 'en-US,en;q=0.8',
+    //        //'Host': 'undefined.local:5000',
+    //        //'Origin': 'http://localhost:9000',
+    //        //'Referer': 'http://localhost:9000/'
+    //    }
+    //};
+
+    //if (options.method === 'POST') {
+    //    postData = JSON.stringify(params.data);
+    //    //options.headers['Content-Length'] = Buffer.byteLength(postData);
+    //    //console.log('post', postData);
+    //}
+
+    //console.log('options', options);
+
+    //return new Promise(function(resolve, reject) {
+    //    var req = http.request(options, function(response) {
+    //        //console.log('perform query', response);
+    //        var bodyString = '';
+    //        response.setEncoding('utf8');
+
+    //        response.on('data', function(chunk) {
+    //            console.log('receiving data...');
+    //            bodyString += chunk;
+    //        });
+
+    //        response.on('end', function() {
+    //            console.log('end', bodyString);
+    //            resolve(bodyString);
+    //        });
+    //    });
+
+    //    console.log('promise');
+
+    //    req.on('error', function(err) {
+    //        console.log('ERR', err);
+    //        reject(err);
+    //    });
+
+    //    req.on('timeout', function() {
+    //        console.log('timeout');
+    //    });
+
+    //    if (options.method === 'POST') {
+    //        //req.write(postData);
+    //        console.log('post', postData);
+    //        req.write(postData);
+    //    }
+
+    //    req.end();
+    //});
 };
 
 Webhook.prototype.fire = function(currentUrl) {
