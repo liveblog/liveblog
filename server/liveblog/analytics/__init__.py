1f24ecdab3ffbfa2b4567d7f11a2b072c94df0a5
daniel.read <daniel.read@sourcefabric.org>
2016-12-06 14:48:28 +0100

    New analytics related files

---
+++ server/liveblog/analytics/__init__.py
@@ -0,0 +1,10 @@
+import superdesk
+from .analytics import AnalyticsResource, AnalyticsService
+
+def init_app(app):
+    # Analytics
+    endpoint_name = 'analytics'
+    service = AnalyticsService(endpoint_name, backend=superdesk.get_backend())
+    AnalyticsResource(endpoint_name, app=app, service=service)
+
+superdesk.privilege(name='analytics', label='Analytics Management', description='User can manage analytics')
