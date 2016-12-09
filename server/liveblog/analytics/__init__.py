b383e0f2b9e6802994655203ff7d8395f0adf975
daniel.read <daniel.read@sourcefabric.org>
2016-12-09 17:54:46 +0100

    Fixed bloganalytics endpoint

--- server/liveblog/analytics/__init__.py
+++ server/liveblog/analytics/__init__.py
@@ -1,5 +1,5 @@
 import superdesk
-from .analytics import AnalyticsResource, AnalyticsService
+from .analytics import AnalyticsResource, AnalyticsService, BlogAnalyticsResource, BlogAnalyticsService
 
 
 def init_app(app):
@@ -7,5 +7,11 @@ def init_app(app):
     endpoint_name = 'analytics'
     service = AnalyticsService(endpoint_name, backend=superdesk.get_backend())
     AnalyticsResource(endpoint_name, app=app, service=service)
+
+    # Blog Analytics
+    endpoint_name = 'blog_analytics'
+    service = BlogAnalyticsService(endpoint_name, backend=superdesk.get_backend())
+    BlogAnalyticsResource(endpoint_name, app=app, service=service)
+
 
 superdesk.privilege(name='analytics', label='Analytics Management', description='User can manage analytics')
