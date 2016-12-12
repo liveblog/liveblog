607d19b345dc51fb35a4b5024ec63c349386122e
Loïc Nogues <nogues.loic@gmail.com>
2016-12-12 17:50:53 +0100

    Added tabs

--- client/app/scripts/liveblog-marketplace/controllers/marketplace.js
+++ client/app/scripts/liveblog-marketplace/controllers/marketplace.js
@@ -1,15 +1,18 @@
 liveblogMarketplace
     .controller('MarketplaceController', ['$scope', function($scope) {
-        $scope.producers = { _items: [
-            {
-                _id: 0,
-                title: 'Deutsche Presse-Argentur',
-                description: 'Lorem ipsum dolor sit amet'
-            },
-            {
-                _id: 1,
-                title: 'Sourcefabric',
-                description: 'Lorem ipsum dolor sit amet'
-            }
-        ]};
+        $scope.states = [
+            { text: 'Available Producers' },
+            { text: 'Registered Producers' }
+        ];
+
+        $scope.producers = { _items: [{
+            _id: 0,
+            title: 'Deutsche Presse-Argentur',
+            description: 'Lorem ipsum dolor sit amet'
+        },
+        {
+            _id: 1,
+            title: 'Sourcefabric',
+            description: 'Lorem ipsum dolor sit amet'
+        }]};
     }]);
