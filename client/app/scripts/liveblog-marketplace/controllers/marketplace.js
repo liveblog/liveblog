49e05bc4a81c8bc30bc6f9dfafff0da7c82c9fa3
Loïc Nogues <nogues.loic@gmail.com>
2016-12-12 17:43:17 +0100

    Producer template

--- client/app/scripts/liveblog-marketplace/controllers/marketplace.js
+++ client/app/scripts/liveblog-marketplace/controllers/marketplace.js
@@ -1,3 +1,15 @@
 liveblogMarketplace
     .controller('MarketplaceController', ['$scope', function($scope) {
+        $scope.producers = { _items: [
+            {
+                _id: 0,
+                title: 'Deutsche Presse-Argentur',
+                description: 'Lorem ipsum dolor sit amet'
+            },
+            {
+                _id: 1,
+                title: 'Sourcefabric',
+                description: 'Lorem ipsum dolor sit amet'
+            }
+        ]};
     }]);
