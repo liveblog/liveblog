aff36a04cf77a7c4dec648cc262e2ea60217ced2
Loïc Nogues <nogues.loic@gmail.com>
2016-12-13 15:19:58 +0100

    Navigation from marketplace to producer

--- client/app/scripts/liveblog-marketplace/controllers/marketplace.js
+++ client/app/scripts/liveblog-marketplace/controllers/marketplace.js
@@ -1,22 +1,31 @@
 liveblogMarketplace
-    .controller('MarketplaceController', ['$scope', '$location', function($scope, $location) {
-        $scope.states = [
-            { text: 'Available Producers' },
-            { text: 'Registered Producers' }
-        ];
+    .controller('MarketplaceController', ['$scope', '$location', 'api',
+        function($scope, $location, api) {
+            $scope.states = [
+                { text: 'Available Producers' },
+                { text: 'Registered Producers' }
+            ];
 
-        $scope.producers = { _items: [{
-            _id: 0,
-            title: 'Deutsche Presse-Argentur',
-            description: 'Lorem ipsum dolor sit amet'
-        },
-        {
-            _id: 1,
-            title: 'Sourcefabric',
-            description: 'Lorem ipsum dolor sit amet'
-        }]};
+            $scope.producers = { _items: [{
+                _id: 0,
+                title: 'Deutsche Presse-Argentur',
+                description: 'Lorem ipsum dolor sit amet'
+            },
+            {
+                _id: 1,
+                title: 'Sourcefabric',
+                description: 'Lorem ipsum dolor sit amet'
+            }]};
 
-        $scope.open = function(producer) {
-            $location.path('/marketplace/' + producer._id);
-        };
-    }]);
+            $scope.open = function(producer) {
+                $location.path('/marketplace/' + producer._id);
+            };
+
+            api('marketplace/marketers').query()
+                .then(function(result) {
+                    console.log('marketers', result)
+                })
+                .catch(function(err) {
+                    console.log('err', err);
+                })
+        }]);
--- client/app/scripts/liveblog-marketplace/controllers/mp-producer.js
+++ client/app/scripts/liveblog-marketplace/controllers/mp-producer.js
@@ -8,6 +8,7 @@ liveblogMarketplace
         $scope.blogs = {_items: [{
             _id: 0,
             title: 'Blog number 1',
-            description: 'Lorem ipsum dolor sit amet'
+            description: 'Lorem ipsum dolor sit amet',
+            url: 'https://liveblog-test-eu-west-1.s3-eu-west-1.amazonaws.com/liveblogsyndtest1/blogs/584530063b0a48003a970b67/index.html'
         }], _meta: { total: 1 }};
     }]);
