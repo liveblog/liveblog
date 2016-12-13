b2a745f65da6670715fbb749ce7f1c41d2dd5296
psolbach <p@psolbach.com>
2016-12-13 17:07:52 +0100

    fix bad resolves

--- client/app/scripts/main.js
+++ client/app/scripts/main.js
@@ -47,7 +47,7 @@ define('main', [
             }]);
         }])
         //show messages on websocket disconnect and connect
-        .run(['$rootScope', '$timeout', 'notify', 'gettext', 'session', function($rootScope, $timeout, notify, gettext, session) {
+        .run(['$rootScope', '$timeout', '$window', 'notify', 'gettext', 'session', function($rootScope, $timeout, $window, notify, gettext, session) {
             var alertTimeout;
             $rootScope.$on('disconnected', function(event) {
                 $timeout.cancel(alertTimeout);
@@ -74,6 +74,7 @@ define('main', [
                 if ((e.keyCode || e.which) === 27) {
                     $window.history.back(); // close on ESC
                 }
+            }
         }]);
         // load apps & bootstrap
         var body = angular.element('body');
