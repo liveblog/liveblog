d5a1fa1bfb1b072bc64c42fe0a9505ade981654b
Loïc Nogues <nogues.loic@gmail.com>
2016-12-13 15:54:59 +0100

    Display iframe

--- client/app/scripts/liveblog-marketplace/controllers/mp-producer.js
+++ client/app/scripts/liveblog-marketplace/controllers/mp-producer.js
@@ -1,5 +1,5 @@
 liveblogMarketplace
-    .controller('MPProducerController', ['$scope', function($scope) {
+    .controller('MPProducerController', ['$scope', '$sce', function($scope, $sce) {
         $scope.states = [
             { text: 'Active Blogs' },
             { text: 'Archived Blogs' }
@@ -9,6 +9,13 @@ liveblogMarketplace
             _id: 0,
             title: 'Blog number 1',
             description: 'Lorem ipsum dolor sit amet',
-            url: 'https://liveblog-test-eu-west-1.s3-eu-west-1.amazonaws.com/liveblogsyndtest1/blogs/584530063b0a48003a970b67/index.html'
+            url: $sce.trustAsResourceUrl('https://liveblog-test-eu-west-1.s3-eu-west-1.amazonaws.com/liveblogsyndtest1/blogs/584530063b0a48003a970b67/index.html')
         }], _meta: { total: 1 }};
+
+        $scope.embedModal = false;
+
+        $scope.openEmbedModal = function(blog) {
+            $scope.embedModal = true;
+            $scope.currentBlog = blog;
+        };
     }]);
