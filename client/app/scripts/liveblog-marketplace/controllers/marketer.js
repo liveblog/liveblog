02ac20dda5001cb472c6d86ee4422a39011f71be
Loïc Nogues <nogues.loic@gmail.com>
2016-12-14 12:24:41 +0100

    Blog modal

--- client/app/scripts/liveblog-marketplace/controllers/marketer.js
+++ client/app/scripts/liveblog-marketplace/controllers/marketer.js
@@ -1,12 +1,20 @@
 liveblogMarketplace
     .controller('MarketerController', ['$scope', '$sce', 'api', '$routeParams',
         function($scope, $sce, api, $routeParams) {
+            var iframeAttrs = [
+                'width="100%"',
+                'height="715"',
+                'frameborder="0"',
+                'allowfullscreen'
+            ].join(' ');
+
             $scope.states = [
                 { text: 'Active Blogs' },
                 { text: 'Archived Blogs' }
             ];
 
             $scope.embedModal = false;
+            $scope.active = 'preview';
 
             $scope.openEmbedModal = function(blog) {
                 $scope.embedModal = true;
@@ -18,6 +26,7 @@ liveblogMarketplace
                     $scope.marketer = { blogs: angular.extend(blogs, {
                         _items: blogs._items.map(function(item) {
                             return angular.extend(item, {
+                                embed: '<iframe '+iframeAttrs+' src="'+item.public_url+'"></iframe>',
                                 public_url: $sce.trustAsResourceUrl(item.public_url)
                             });
                         })
