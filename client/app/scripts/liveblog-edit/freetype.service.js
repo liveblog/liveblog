35b651bb44e18e4fd804ae5aefa5f51a64991e61
darconny <darko.tomic@gmail.com>
2017-01-05 11:37:43 +0100

    Scorecards UI work

--- client/app/scripts/liveblog-edit/freetype.service.js
+++ client/app/scripts/liveblog-edit/freetype.service.js
@@ -411,7 +411,7 @@
     .directive('freetypeCollectionAdd', ['$compile', function($compile) {
         return {
             restrict: 'E',
-            template: '<button ng-click="ftca.add()">+</button>',
+            template: '<button ng-click="ftca.add()" class="scorecard-plus-btn">+</button>',
             controller: ['$scope', function($scope) {
                 this.add = function() {
                     $scope.vector.push({});
@@ -426,7 +426,7 @@
     .directive('freetypeCollectionRemove', function() {
         return {
             restrict: 'E',
-            template: '<button ng-click="ftcr.remove()" ng-show="vector.length!==1">-</button>',
+            template: '<button ng-click="ftcr.remove()" ng-show="vector.length!==1" class="scorecard-plus-btn">-</button>',
             controller: ['$scope', function($scope) {
                 this.remove = function() {
                     $scope.vector.splice($scope.index, 1);
