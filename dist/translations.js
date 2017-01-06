f721e4cc5d0a52c3da7c89df8922eea5a611d5c4
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-01-06 12:13:55 +0200

    Squashed 'server/liveblog/themes/themes_assets/classic/' changes from 489642d..0b8b7aa
    0b8b7aa feature: added on item file group_type check.
    86d0eba Merge pull request #26 from liveblog/finnish-translation
    d5c40c3 chore: added Finnish language and build.
    61f9f1b Finnish translation of Classic theme
    5489d5b Merge pull request #25 from liveblog/german-translation-fix
    61012ee chore: build
    b141bf4 kredit --> Bild
    5a21130 Merge pull request #24 from darconny/lb-icon-font
    e83e8ca lb-icon-font
    bbdd329 fix: i18n changed from `pined` to `pinned`.
    d1af672 fix: css style for norsk.
    a785cd3 chore: build
    9909994 fix: `nb` country code to `no`
    60ac84a Merge pull request #22 from liveblog/newtranslations
    259da67 Merge pull request #23 from liveblog/addedtranslations
    01538b3 New translations added
    5017c48 CS and NO translations
    c13bfa6 Merge pull request #21 from liveblog/improvement/more-flexibility
    74087ee LBSD-1084 Allow for more flexibility in extending the theme
    
    git-subtree-dir: server/liveblog/themes/themes_assets/classic
    git-subtree-split: 0b8b7aa1b83184a8bdcb8c3837fafc47ed1967fa

--- dist/translations.js
+++ dist/translations.js
@@ -7,19 +7,34 @@ angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
   "Comment": "Váš příspěvek",
   "Comment *": "Text *",
   "Comment should be maximum 300 characters in length.": "Maximální délka textu je 300 znaků.",
-  "Editorial": "podle redakce",
+  "Editorial": "redakční",
   "Load more posts": "Načíst další",
   "Loading": "Načítám",
   "Name *": "Jméno *",
   "Name should be maximum 30 characters in length.": "Maximální délka jména je 30 znaků.",
-  "Newest first": "od nejnovějších",
-  "Oldest first": "od nejstarších",
+  "Newest first": "nejnovější",
+  "No posts for now.": "Žádné příspěvky.",
+  "Oldest first": "nejstarší",
+  "One pinned post": [
+    "Jeden připnutý příspěvek",
+    "{{$count}} připnuté příspěvky",
+    "{{$count}} připnutých příspěvků"
+  ],
   "Please fill in your Comment.": "Napište váš text.",
   "Please fill in your Name.": "Napište své jméno.",
   "Post a comment": "Otázka / komentář",
+  "See one new update": [
+    "Zobraz 1 nový příspěvek",
+    "Zobraz {{$count}} nové příspěvky",
+    "Zobraz {{$count}} nových příspěvků"
+  ],
   "Send": "Odeslat",
+  "Show all posts": "Zobrazit všechny",
+  "Show highlighted post only": "Zobraz jen zvýrazněné příspěvky",
   "Sort by:": "Řazení:",
-  "Your comment was sent for approval.": "Váš text byl úspěšně odeslán. Čeká na schválení."
+  "Updated {{post.content_updated_date | prettifyIsoDate}}": "Aktualizace {{post.content_updated_date | prettifyIsoDate}}",
+  "Your comment was sent for approval.": "Váš text byl úspěšně odeslán. Čeká na schválení.",
+  "{{ item.meta.caption }} credit: {{ item.meta.credit }}": "{{ item.meta.caption }} autor: {{ item.meta.credit }}"
 });
   gettextCatalog.setStrings('de', {
   "Cancel": "Abbrechen",
@@ -51,7 +66,39 @@ angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
   "Sort by:": "Ordnen nach",
   "Updated {{post.content_updated_date | prettifyIsoDate}}": "Aktualisiert am {{post.content_updated_date | prettifyIsoDate}}",
   "Your comment was sent for approval.": "Ihr Kommentar wartet auf Freischaltung,",
-  "{{ item.meta.caption }} credit: {{ item.meta.credit }}": "{{ item.meta.caption }} kredit: {{ item.meta.credit }}"
+  "{{ item.meta.caption }} credit: {{ item.meta.credit }}": "{{ item.meta.caption }} Bild: {{ item.meta.credit }}"
+});
+  gettextCatalog.setStrings('fi', {
+  "Cancel": "Peruuta",
+  "Comment": "Kommentoi",
+  "Comment *": "Kommentti *",
+  "Comment should be maximum 300 characters in length.": "Kommentin enimmäispituus on 300 merkkiä.",
+  "Editorial": "Toimituksellinen",
+  "Load more posts": "Lataa lisää julkaisuja",
+  "Loading": "Lataa",
+  "Name *": "Nimi *",
+  "Name should be maximum 30 characters in length.": "Nimen enimmäispituus on 30 merkkiä.",
+  "Newest first": "Uusimmat ensin",
+  "No posts for now.": "Ei uusia julkaisuja.",
+  "Oldest first": "Vanhimmat ensin",
+  "One pinned post": [
+    "Yksi kiinnitetty julkaisu",
+    "{{$count}} kiinnitettyä julkaisua"
+  ],
+  "Please fill in your Comment.": "Lisää kommenttisi.",
+  "Please fill in your Name.": "Lisää nimesi.",
+  "Post a comment": "Lähetä kommentti",
+  "See one new update": [
+    "Lataa yksi uusi julkaisu",
+    "Lataa {{$count}} uutta julkaisua"
+  ],
+  "Send": "Lähetä",
+  "Show all posts": "Näytä kaikki julkaisut",
+  "Show highlighted post only": "Näytä vain korostettu julkaisu",
+  "Sort by:": "Järjestä:",
+  "Updated {{post.content_updated_date | prettifyIsoDate}}": "Päivitetty {{post.content_updated_date | prettifyIsoDate}}",
+  "Your comment was sent for approval.": "Kommenttisi lähetettiin hyväksyttäväksi.",
+  "{{ item.meta.caption }} credit: {{ item.meta.credit }}": "{{ item.meta.caption }} © {{ item.meta.credit }}"
 });
   gettextCatalog.setStrings('fr', {
   "Cancel": "Annuler",
@@ -64,10 +111,19 @@ angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
   "Name *": "Nom *",
   "Name should be maximum 30 characters in length.": "Le nom ne peut excéder 30 signes",
   "Newest first": "Le plus récent d'abord",
+  "No posts for now.": "Aucun message pour le moment.",
   "Oldest first": "Plus ancien en premier",
+  "One pinned post": [
+    "Voir le nouveau message",
+    "Voir {{$count}} nouveaux messages"
+  ],
   "Please fill in your Comment.": "Votre commentaire.",
   "Please fill in your Name.": "Votre nom.",
   "Post a comment": "Envoyer un commentaire",
+  "See one new update": [
+    "Voir le nouveau message",
+    "Voir {{$count}} nouveaux messages"
+  ],
   "Send": "Envoyer",
   "Sort by:": "Trier par:",
   "Your comment was sent for approval.": "Votre commentaire a été envoyé et est en attente de validation."
@@ -83,13 +139,54 @@ angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
   "Name *": "Naam *",
   "Name should be maximum 30 characters in length.": "Uw naam kan maximaal 30 tekens lang zijn.",
   "Newest first": "Toon nieuwste eerst",
+  "No posts for now.": "Nog geen berichten beschikbaar.",
   "Oldest first": "Toon oudste eerst",
+  "One pinned post": [
+    "Bekijk nieuw bericht",
+    "Bekijk {{$count}} nieuwe berichten"
+  ],
   "Please fill in your Comment.": "Uw reactie.",
   "Please fill in your Name.": "Vul hier uw naam in.",
   "Post a comment": "Schrijf een reactie",
+  "See one new update": [
+    "Bekijk nieuw bericht",
+    "Bekijk {{$count}} nieuwe berichten"
+  ],
   "Send": "Verzenden",
   "Sort by:": "Sorteer:",
   "Your comment was sent for approval.": "Uw reactie is ontvangen ter beoordeling."
+});
+  gettextCatalog.setStrings('no', {
+  "Cancel": "Avbryt",
+  "Comment": "Kommentar",
+  "Comment *": "Kommentar*",
+  "Comment should be maximum 300 characters in length.": "Kommentarer kan være inntil 300 tegn",
+  "Editorial": "Redaksjonelt",
+  "Load more posts": "Henter flere poster",
+  "Loading": "Henter",
+  "Name *": "Navn*",
+  "Name should be maximum 30 characters in length.": "Navn kan ikke ha mer enn 30 tegn",
+  "Newest first": "Nyeste først",
+  "No posts for now.": "Ingen poster for øyeblikket",
+  "Oldest first": "Eldste først",
+  "One pinned post": [
+    "Én post festet til toppen",
+    "{{$count}} poster festet til toppen"
+  ],
+  "Please fill in your Comment.": "Skriv inn din kommentar",
+  "Please fill in your Name.": "Skriv inn navn",
+  "Post a comment": "Post en kommentar",
+  "See one new update": [
+    "Se én ny oppdatering",
+    "Se {{$count}} nye oppdateringer"
+  ],
+  "Send": "Send",
+  "Show all posts": "Vis alle poster",
+  "Show highlighted post only": "Vis bare høydepunkter",
+  "Sort by:": "Sortér etter:",
+  "Updated {{post.content_updated_date | prettifyIsoDate}}": "Oppdatert {{post.content_updated_date | prettifyIsoDate}}",
+  "Your comment was sent for approval.": "Din kommentar er sendt til godkjenning",
+  "{{ item.meta.caption }} credit: {{ item.meta.credit }}": "{{ item.meta.caption }} credit: {{ item.meta.credit }}"
 });
   gettextCatalog.setStrings('ro', {
   "Cancel": "Anulează",
@@ -102,10 +199,21 @@ angular.module('gettext').run(['gettextCatalog', function (gettextCatalog) {
   "Name *": "Numele *",
   "Name should be maximum 30 characters in length.": "Numele nu poate fi mai lung de 30 de caractere.",
   "Newest first": "Cele mai noi",
+  "No posts for now.": "Deocamdata nu sunt articole.",
   "Oldest first": "Cele mai vechi",
+  "One pinned post": [
+    "Vezi un articol nou",
+    "Vezi {{$count}} articole noi",
+    "Vezi {{$count}} de articole noi"
+  ],
   "Please fill in your Comment.": "Completează comentariu.",
   "Please fill in your Name.": "Completează numele.",
   "Post a comment": "Scrie un comentariu",
+  "See one new update": [
+    "Vezi un articol nou",
+    "Vezi {{$count}} articole noi",
+    "Vezi {{$count}} de articole noi"
+  ],
   "Send": "Trimite",
   "Sort by:": "Ordonează după:",
   "Your comment was sent for approval.": "Comentariul tău a fost trimis spre aprobare."
