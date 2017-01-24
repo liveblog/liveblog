f58b0aab9a036aecce97c62bbada374ad4bc8cba
Massimo Scamarcia <massimo.scamarcia@sourcefabric.org>
2017-01-24 16:58:41 +0100

    trying to change path.repo for elasticsearch

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-mkdir /tmp/es-backups
-sudo chown elasticsearch:elasticsearch /tmp/es-backups
-echo "path.repo: ['/tmp/es-backups']" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+sudo mkdir /var/backups/es-backups
+sudo chown elasticsearch:elasticsearch /var/backups/es-backups
+echo "path.repo: [\"/var/backups/es-backups\"]" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "index.store.type: memory" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "node.name: \"test_node\"" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "network.host: 0.0.0.0" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "http.port: 9200" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "discovery.zen.ping.multicast.enabled: false" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
-echo "discovery.zen.ping.unicast.hosts: [\"127.0.0.1\", \"127.0.0.1:9200\"]" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 sudo service elasticsearch restart
 tail -n1 /etc/elasticsearch/elasticsearch.yml
 sleep 10
