d6cd704acd08135f1d7be44996012cda385c9764
Massimo Scamarcia <massimo.scamarcia@sourcefabric.org>
2017-01-24 15:56:59 +0100

    updated travis and elasticsearch conf

--- scripts/elastic.sh
+++ scripts/elastic.sh
@@ -3,6 +3,9 @@ mkdir /tmp/es-backups
 sudo chown elasticsearch:elasticsearch /tmp/es-backups
 echo "path.repo: ['/tmp/es-backups']" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 echo "index.store.type: memory" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+echo "node.name: \"test_node\"" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+echo "network.host: 0.0.0.0" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
+echo "http.port: 9200" | sudo tee -a /etc/elasticsearch/elasticsearch.yml
 sudo service elasticsearch restart
 tail -n1 /etc/elasticsearch/elasticsearch.yml
 sleep 10
