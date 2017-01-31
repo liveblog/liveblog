b5846ba3cab1c7e0a207a2140541854132ec822d
Loïc Nogues <nogues.loic@gmail.com>
2017-01-31 12:25:20 +0100

    Sidebar styling

--- client/app/scripts/liveblog-marketplace/directives/search-panel.js
+++ client/app/scripts/liveblog-marketplace/directives/search-panel.js
@@ -3,10 +3,17 @@ liveblogMarketplace
         return {
             templateUrl: 'scripts/liveblog-marketplace/views/search-panel.html',
             link: function(scope) {
+                scope.categories = [
+                    "Breaking News",
+                    "Entertainment",
+                    "Business and Finance",
+                    "Sport",
+                    "Technology"
+                ];
+
                 api.get('/marketplace/marketers')
                     .then(function(marketers) {
                         scope.marketers = marketers;
-                        console.log(marketers);
                     });
             }
         };
