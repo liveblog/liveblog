4684517f8b7cf3f152bbeb0dc6ee7872c722d169
Loïc Nogues <nogues.loic@gmail.com>
2017-02-02 15:38:20 +0100

    Removing useless controllers

--- client/app/scripts/liveblog-marketplace/index.js
+++ client/app/scripts/liveblog-marketplace/index.js
@@ -11,15 +11,7 @@ liveblogMarketplace
                 priority: 100,
                 adminTools: true,
                 resolve: {isArchivedFilterSelected: function() {return false;}}
-            })
-            .activity('/marketplace/:type/:id', {
-                label: gettext('Marketplace'),
-                controller: 'MarketerController',
-                templateUrl: 'scripts/liveblog-marketplace/views/marketer.html',
-                category: superdesk.MENU_MAIN,
-                priority: 100,
-                resolve: {isArchivedFilterSelected: function() {return false;}}
-             });
+            });
     //}])
     //.config(['apiProvider', function(apiProvider) {
     //    apiProvider
