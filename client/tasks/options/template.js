bd97a4c58d9d49e4d28b100d777a1723a19ef347
Loïc Nogues <nogues.loic@gmail.com>
2017-02-06 11:12:13 +0100

    Added subscrtiption level variable

--- client/tasks/options/template.js
+++ client/tasks/options/template.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
             },
             facebookAppId: grunt.option('facebook-appid') || process.env.FACEBOOK_APP_ID || '',
             syndication: process.env.SYNDICATION || false,
+            subscriptionLevel: process.env.SUBSCRIPTION_LEVEL || '',
             analytics: {
                 piwik: {
                     url: process.env.PIWIK_URL || '',
