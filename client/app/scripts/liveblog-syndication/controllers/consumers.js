ad9eddeea61ddb0c30832887f5b8eaffa417b268
Loïc Nogues <nogues.loic@gmail.com>
2017-02-15 14:22:59 +0100

    Moved consumers in a separate directive

--- client/app/scripts/liveblog-syndication/controllers/consumers.js
+++
@@ -1,7 +0,0 @@
-liveblogSyndication
-    .controller('ConsumersController', ['$scope', '$controller', function($scope, $controller) {
-        $scope.endPoint = 'consumers';
-        $scope.entryName = 'consumer';
-
-        angular.extend(this, $controller('BaseController', {$scope: $scope}));
-    }]);
