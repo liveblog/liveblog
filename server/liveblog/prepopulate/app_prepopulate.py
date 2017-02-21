c7213499326c11ccb685adf14a95e4a863d00d32
Massimo Scamarcia <massimo.scamarcia@sourcefabric.org>
2017-02-21 10:41:16 +0100

    Added missing wooper package dependency.

--- server/liveblog/prepopulate/app_prepopulate.py
+++ server/liveblog/prepopulate/app_prepopulate.py
@@ -182,4 +182,5 @@ class AppPrepopulateCommand(superdesk.Command):
             get_resource_service('users').post([get_default_user()])
         prepopulate_data(prepopulate_file, get_default_user())
 
+
 superdesk.command('app:prepopulate', AppPrepopulateCommand())
