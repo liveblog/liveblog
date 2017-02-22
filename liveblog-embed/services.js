fe0b2a5703e51bc0f62863dd352dd86cd7b62914
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-02-22 12:07:57 +0200

    Squashed 'server/liveblog/themes/themes_assets/angular/' changes from 82361c6..417101b
    417101b Merge pull request #24 from liveblog/fix/theme-template-requests
    e69818b Merge branch 'fix/theme-template-requests' of github.com:liveblog/lb-theme-angular into fix/theme-template-requests
    bcc684d chore: build
    d16a2f2 fix: stop makeing template requests that are cached.
    9f26470 Merge pull request #25 from liveblog/LBSD-1324
    00f0f32 Updated theme version
    f16aa79 Create a cookie after sending a post to the analytics
    066b6e2 Hit the analytics
    e322eef fix: stop makeing template requests that are cached.
    3febee6 Merge pull request #23 from liveblog/feature/filters-enhance
    
    git-subtree-dir: server/liveblog/themes/themes_assets/angular
    git-subtree-split: 417101b7e23f92317af221d1c4cc9e46b5008e9d

--- liveblog-embed/services.js
+++ liveblog-embed/services.js
@@ -14,7 +14,16 @@
         .provider('asset', [ '$injector', function ($injector) {
             this.templateUrl = function(path) {
                 var config = $injector.get('config'),
+                    simplifiedPath = $injector.has('assets_simplified_path')
+                            && $injector.get('assets_simplified_path'),
                     ret = path;
+                /**
+                 * `assets_simplified_path` constant is added 
+                 * to keep backwards compatibility for old themes.
+                 */ 
+                if(!config.debug && simplifiedPath) {
+                    return ret;
+                }
                 if (!/^(https?:\/\/|\/\/)/.test(path)) {
                     ret = config.assets_root +'/' + ret;
                 }
