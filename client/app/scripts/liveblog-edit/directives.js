3f2dc157b19e828c0ac93ba841084944fbcb9c03
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-02-28 12:48:30 +0200

    fix: removed commented code.

--- client/app/scripts/liveblog-edit/directives.js
+++ client/app/scripts/liveblog-edit/directives.js
@@ -599,7 +599,6 @@ define([
                 restrict: 'E',
                 templateUrl: 'scripts/liveblog-edit/views/freetype-link.html',
                 controller: ['$scope', function($scope) {
-                    // $scope.link = 'http://';
                     var regex = /https?:\/\/[^\s]+\.[^\s\.]+/;
                     $scope._id = _.uniqueId('link');
                     var sentinel = $scope.$watch('link', function(value) {
