e2c8a611a1d156a907c05dfd270bd22f779afa48
Loïc Nogues <nogues.loic@gmail.com>
2017-03-14 10:47:21 +0100

    Refactoring service requirement

--- client/app/scripts/liveblog-edit/index.js
+++ client/app/scripts/liveblog-edit/index.js
@@ -4,10 +4,21 @@ import './styles/posts-in-panel.scss';
 import './styles/settings.scss';
 import './styles/timeline.scss';
 
-import './posts.service';
-import './unread.posts.service';
-import './blog.service';
+import postsService from './posts.service';
+import unreadPostsService from './unread.posts.service';
+import blogService from './blog.service';
+
+angular.module('liveblog.posts', [])
+    .service('postsService', postsService)
+    .service('unreadPostsService', unreadPostsService);
+
+angular.module('liveblog.blog', [])
+    .service('blogService', blogService);
+
 import './pages-manager.service';
+
+//angular.module('liveblog.pages-manager', ['liveblog.posts', 'liveblog.edit'])
+
 import './freetype.service';
 import './module';
 import './directives';
