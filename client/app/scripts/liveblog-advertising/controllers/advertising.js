bbd43a65d9313579737c54845e74f98c4d45e6c2
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2017-03-21 20:43:22 +0200

    with ad advert modal

--- client/app/scripts/liveblog-advertising/controllers/advertising.js
+++ client/app/scripts/liveblog-advertising/controllers/advertising.js
@@ -1,11 +1,49 @@
-LiveblogAdvertisingController.$inject = ['$scope', 'api', '$location', 'notify', 'gettext',
-'$q', '$sce', 'config', 'lodash', 'upload', 'blogService', 'modal', '$templateCache'];
+import adsLocalTpl from 'scripts/liveblog-edit/views/ads-local.html';
+import adsRemoteTpl from 'scripts/liveblog-edit/views/ads-remote.html';
 
-export default function LiveblogAdvertisingController($scope, api, $location, notify, gettext,
-$q, $sce, config, _, upload, blogService, modal, $templateCache) {
+LiveblogAdvertisingController.$inject = ['$scope', 'api', 'notify', 'gettext',
+'upload','$templateCache', 'freetypeService'];
+
+export default function LiveblogAdvertisingController($scope, api, notify, gettext,
+upload, $templateCache, freetypeService) {
     var vm = this;
     $scope.activeState = 'adverts';
+    $scope.advertType = '';
+    $scope.advert = {};
+    $scope.advertModalActive = false;
+    $scope.dialogAdvertLoading = false;
+
+
+    $scope.freetypesData = {}; $scope.freetypeControl = {}; $scope.validation = {};
+    $scope.adTypes = [
+    	{
+            name: 'Advertisement Local',
+            template: $templateCache.get(adsLocalTpl)
+        },
+        {
+            name: 'Advertisement Remote',
+            template: $templateCache.get(adsRemoteTpl)
+        }
+    ]
+
     $scope.changeState = function(state) {
         $scope.activeState = state;
+    }
+    $scope.createNew = function(ad) {
+    	$scope.advertModalActive = true;
+    	$scope.advertType = ad;
+    }
+    $scope.cancelAdvertCreate = function() {
+    	$scope.freetypeControl.reset();
+    	$scope.advertModalActive = false;
+    }
+    $scope.saveAdvert = function() {
+    	var newAd = {
+    		name: $scope.advert.name,
+    		type: $scope.advertType.name,
+    		text: freetypeService.htmlContent($scope.advertType.template, $scope.freetypesData),
+    		meta: {data: $scope.freetypesData}
+    	}
+    	console.log(newAd);
     }
 }
