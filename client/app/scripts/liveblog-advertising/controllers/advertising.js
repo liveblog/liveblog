741da43d33de2e55b6dc7f636599c4525b949860
Vlad Nicoara <vlad.nicoara@sourcefabric.org>
2017-03-22 00:08:45 +0200

    fixed mixed tabs and spaces for identation

--- client/app/scripts/liveblog-advertising/controllers/advertising.js
+++ client/app/scripts/liveblog-advertising/controllers/advertising.js
@@ -6,7 +6,6 @@ LiveblogAdvertisingController.$inject = ['$scope', 'api', 'notify', 'gettext',
 
 export default function LiveblogAdvertisingController($scope, api, notify, gettext,
 upload, $templateCache, freetypeService, modal) {
-    var vm = this;
     $scope.activeState = 'adverts';
     $scope.advertType = '';
     $scope.advert = {};
@@ -17,7 +16,7 @@ upload, $templateCache, freetypeService, modal) {
 
     $scope.freetypesData = {}; $scope.freetypeControl = {}; $scope.validation = {};
     $scope.adTypes = [
-    	{
+        {
             name: 'Advertisement Local',
             template: $templateCache.get(adsLocalTpl)
         },
@@ -31,44 +30,44 @@ upload, $templateCache, freetypeService, modal) {
         $scope.activeState = state;
     }
     $scope.openAdvertDialog = function(ad) {
-    	if (ad._id) {
-    		// editing advert
-    		$scope.advert = angular.copy(ad);
-    		angular.forEach($scope.adTypes, function(adType) {
-    			if (adType.name === ad.type) {
-    				$scope.advertType = adType;
-    			}
-    		});
-    		$scope.freetypesData = angular.copy(ad.meta.data);
-    	} else {
-    		$scope.advert = {};
-    		$scope.advertType = ad;
-    	}
-    	$scope.advertModalActive = true;
+        if (ad._id) {
+            // editing advert
+            $scope.advert = angular.copy(ad);
+            angular.forEach($scope.adTypes, function(adType) {
+                if (adType.name === ad.type) {
+                    $scope.advertType = adType;
+                }
+            });
+            $scope.freetypesData = angular.copy(ad.meta.data);
+        } else {
+            $scope.advert = {};
+            $scope.advertType = ad;
+        }
+        $scope.advertModalActive = true;
     }
     $scope.cancelAdvertCreate = function() {
-    	$scope.freetypeControl.reset();
-    	$scope.advertModalActive = false;
+        $scope.freetypeControl.reset();
+        $scope.advertModalActive = false;
     }
 
     function loadAdverts() {
-    	$scope.advertsLoading = true;
-    	api('advertisements').query({deleted:false}).then(function(data) {
+        $scope.advertsLoading = true;
+        api('advertisements').query({deleted: false}).then(function(data) {
             $scope.adverts = data._items;
             notify.info('Adverts loaded');
             $scope.advertsLoading = false;
         }, function(data) {
-        	$scope.advertsLoading = false;
+            $scope.advertsLoading = false;
             notify.error(gettext('There was an error getting the adverts'));
         })
     }
 
     function handleSaveSuccess() {
-		notify.info(gettext('Advert saved successfully'));
-		$scope.freetypeControl.reset();
-    	$scope.advertModalActive = false;
-    	$scope.dialogAdvertLoading = false;
-    	loadAdverts();
+        notify.info(gettext('Advert saved successfully'));
+        $scope.freetypeControl.reset();
+        $scope.advertModalActive = false;
+        $scope.dialogAdvertLoading = false;
+        loadAdverts();
     }
 
     $scope.removeAdvert = function (advert, $index) {
@@ -82,32 +81,32 @@ upload, $templateCache, freetypeService, modal) {
     }
 
     function handleSaveError() {
-    	notify.error(gettext('Something went wrong, please try again later!'), 5000)
+        notify.error(gettext('Something went wrong, please try again later!'), 5000)
     }
 
     $scope.saveAdvert = function() {
-    	var newAd = {
-    		name: $scope.advert.name,
-    		type: $scope.advertType.name,
-    		text: freetypeService.htmlContent($scope.advertType.template, $scope.freetypesData),
-    		meta: {data: $scope.freetypesData}
-    	}
-    	$scope.dialogAdvertLoading = true;
+        var newAd = {
+            name: $scope.advert.name,
+            type: $scope.advertType.name,
+            text: freetypeService.htmlContent($scope.advertType.template, $scope.freetypesData),
+            meta: {data: $scope.freetypesData}
+        }
+        $scope.dialogAdvertLoading = true;
 
-    	if ($scope.advert._id) {
-    		// we are aditing existing ad
-    		api('advertisements').save($scope.advert, newAd).then(function(data) {
-	    		handleSaveSuccess();
-	    	}, function(data) {
-	    		handleSaveError();
-	    	});
-    	} else {
-    		api('advertisements').save(newAd).then(function(data) {
-	    		handleSaveSuccess();
-	    	}, function(data) {
-	    		handleSaveError();
-	    	});
-    	}
+        if ($scope.advert._id) {
+            // we are aditing existing ad
+            api('advertisements').save($scope.advert, newAd).then(function(data) {
+                handleSaveSuccess();
+            }, function(data) {
+                handleSaveError();
+            });
+        } else {
+            api('advertisements').save(newAd).then(function(data) {
+                handleSaveSuccess();
+            }, function(data) {
+                handleSaveError();
+            });
+        }
     }
 
     loadAdverts();
