cd294ef9ef4b890e67d50d0c61b3450b5e01764d
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-03-29 16:04:05 +0300

    feature: added outputs resources.

--- server/liveblog/advertisements/__init__.py
+++ server/liveblog/advertisements/__init__.py
@@ -1,7 +1,7 @@
 import superdesk
 from liveblog.advertisements.advertisements import AdvertisementsResource, AdvertisementsService
 from liveblog.advertisements.collections import CollectionsResource, CollectionsService
-
+from liveblog.advertisements.outputs import OutputsResource, OutputsService
 
 def init_app(app):
     endpoint_name = 'advertisements'
@@ -11,3 +11,7 @@ def init_app(app):
     endpoint_name = 'collections'
     service = CollectionsService(endpoint_name, backend=superdesk.get_backend())
     CollectionsResource(endpoint_name, app=app, service=service)
+
+    endpoint_name = 'outputs'
+    service = OutputsService(endpoint_name, backend=superdesk.get_backend())
+    OutputsResource(endpoint_name, app=app, service=service)
