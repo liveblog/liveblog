fd1ba511c10c79070bd7e8cea8f326290d7f3555
Loïc Nogues <nogues.loic@gmail.com>
2017-04-26 14:37:19 +0200

    updated eslintrc

--- gulpfile.js
+++ gulpfile.js
@@ -57,7 +57,7 @@ gulp.task('lint', () => {
   // So, it's best to have gulp ignore the directory as well.
   // Also, Be sure to return the stream from the task;
   // Otherwise, the task may end before the stream has finished.
-  return gulp.src(['**/*.js','!node_modules/**'])
+  return gulp.src(['js/**/*.js','!node_modules/**'])
     // eslint() attaches the lint output to the "eslint" property
     // of the file object so it can be used by other modules.
     .pipe(eslint({ quiet: true }))
