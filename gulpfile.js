94fdb330e2de81af2e93faa892645a3af21823c8
Loïc Nogues <nogues.loic@gmail.com>
2017-04-26 14:18:59 +0200

    eslint superdesk style

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var gulp = require('gulp')
   , del = require('del')
   , minimist = require('minimist')
   , fs = require('fs')
+  , eslint = require('gulp-eslint')
   , nunjucks_extensions = require('./js/nunjucks_extensions');
 
 
@@ -51,6 +52,22 @@ function loadThemeJSON() {
   });
 }
 
+gulp.task('lint', () => {
+  // ESLint ignores files with "node_modules" paths.
+  // So, it's best to have gulp ignore the directory as well.
+  // Also, Be sure to return the stream from the task;
+  // Otherwise, the task may end before the stream has finished.
+  return gulp.src(['**/*.js','!node_modules/**'])
+    // eslint() attaches the lint output to the "eslint" property
+    // of the file object so it can be used by other modules.
+    .pipe(eslint({ quiet: true }))
+    // eslint.format() outputs the lint results to the console.
+    // Alternatively use eslint.formatEach() (see Docs).
+    .pipe(eslint.format())
+    // To have the process exit with an error code (1) on
+    // lint error, return the stream and pipe to failAfterError last.
+    .pipe(eslint.failAfterError());
+});
 
 // Browserify.
 gulp.task('browserify', ['clean-js'], function(cb) {
