0d313419c092c3094d02fa6e3e170223bb86b2a7
psolbach <p@psolbach.com>
2017-04-28 15:59:59 +0200

    re-add periodic polling

--- js/theme/index.js
+++ js/theme/index.js
@@ -17,6 +17,10 @@ module.exports = {
     handlers.events.attach(); // Register Event, Message Handlers
     viewmodel.init();
 
+    // setInterval(function() {
+    //   viewmodel.loadPosts().then(view.renderPosts); // Start polling
+    // }, 10*1000)
+
     setInterval(() => {
       view.updateTimestamps(); // Convert ISO dates to timeago
     }, 1000);
