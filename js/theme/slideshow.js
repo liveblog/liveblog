5ae1222ebb1e96bb62cd6f2fddd40c149dfd9500
Massimo Scamarcia <massimo.scamarcia@gmail.com>
2017-06-29 17:12:53 +0200

    Squashed 'server/liveblog/themes/themes_assets/default/' content from commit ffdbb61
    git-subtree-dir: server/liveblog/themes/themes_assets/default
    git-subtree-split: ffdbb6186198c35818c6c142ce66fb33e6742684

---
+++ js/theme/slideshow.js
@@ -0,0 +1,154 @@
+const templates = require('./templates');
+
+class Slideshow {
+  constructor() {
+    this.start = this.start.bind(this);
+    this.keyboardListener = this.keyboardListener.bind(this);
+    this.setFocus = this.setFocus.bind(this);
+    this.launchIntoFullscreen = this.launchIntoFullscreen.bind(this);
+    this.exitFullscreen = this.exitFullscreen.bind(this);
+    this.toggleFullscreen = this.toggleFullscreen.bind(this);
+    this.addEventListeners = this.addEventListeners.bind(this);
+  }
+
+  start(e) {
+    let items = [];
+
+    this.iterations = 0;
+    this.isFullscreen = false;
+
+    e.target
+      .closest('article.slideshow')
+      .querySelectorAll('.lb-item img')
+      .forEach((img) => {
+        let matches = [];
+
+        img.getAttribute('srcset').replace(/(\S+)\s\d+w/g, (s, match) => {
+          matches.push(match);
+        });
+
+        let [baseImage, thumbnail, viewImage] = matches;
+
+        items.push({
+          item: {
+            meta: {
+              media: {renditions: {
+                baseImage: {href: baseImage},
+                thumbnail: {href: thumbnail},
+                viewImage: {href: viewImage}
+              }},
+              caption: img.parentNode.querySelector('span.caption').textContent,
+              credit: img.parentNode.querySelector('span.credit').textContent,
+            },
+            active: thumbnail === e.target.getAttribute('src')
+          }
+        });
+      });
+
+    let slideshow = templates.slideshow({
+      refs: items
+    });
+
+    document.querySelector('div.lb-timeline')
+      .insertAdjacentHTML('afterend', slideshow);
+
+    if (window.self !== window.top) {
+      window.parent.postMessage('fullscreen', window.document.referrer);
+    }
+
+    this.setFocus();
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    window.addEventListener('keydown', this.keyboardListener);
+
+    document
+      .querySelector('#slideshow button.fullscreen')
+      .addEventListener('click', this.toggleFullscreen);
+
+    document
+      .querySelector('#slideshow button.arrows.next')
+      .addEventListener('click', () => this.keyboardListener({keyCode: 39}));
+
+    document
+      .querySelector('#slideshow button.arrows.prev')
+      .addEventListener('click', () => this.keyboardListener({keyCode: 37}));
+  }
+
+  toggleFullscreen() {
+    if (!this.isFullscreen) {
+      this.launchIntoFullscreen(document.getElementById('slideshow'));
+    } else {
+      this.exitFullscreen();
+    }
+  }
+
+  launchIntoFullscreen(element) {
+    if (element.requestFullscreen) {
+      element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+      element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+      element.msRequestFullscreen();
+    }
+
+    this.isFullscreen = true;
+  }
+
+  exitFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+
+    this.isFullscreen = false;
+  }
+
+  setFocus() {
+    const container = document.querySelector('#slideshow .container');
+
+    container.querySelectorAll('img').forEach((img, i) => {
+      if (img.classList.contains('active')) {
+        this.iterations = i;
+      }
+    });
+
+    if (this.iterations > 0) {
+      container.style.marginTop = `-${container.offsetHeight * this.iterations}px`;
+    }
+  }
+
+  keyboardListener(e) {
+    const container = document.querySelector('#slideshow .container');
+    const picturesCount = container.querySelectorAll('img').length;
+    let offset = container.offsetHeight * this.iterations;
+
+    switch (e.keyCode) {
+    case 39: // right
+      if (offset + container.offsetHeight < picturesCount * container.offsetHeight) {
+        container.style.marginTop = `-${offset + container.offsetHeight}px`;
+        this.iterations++;
+      }
+
+      break;
+    case 37: // left
+      if (offset - container.offsetHeight >= 0) {
+        container.style.marginTop = `-${offset - container.offsetHeight}px`;
+        this.iterations--;
+      }
+
+      break;
+    case 27: // esc
+      this.exitFullscreen();
+      document.querySelector('#slideshow').remove();
+    }
+  }
+}
+
+module.exports = Slideshow;
