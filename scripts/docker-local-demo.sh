8c0632bfbd475d10f139a180dd516ca77af200dd
Rafał Muszyński <rmuszynski1@gmail.com>
2017-10-13 16:39:25 +0200

    Update docker-local-demo.sh
    Make it working also on OSX

--- scripts/docker-local-demo.sh
+++ scripts/docker-local-demo.sh
@@ -3,22 +3,22 @@
 set -ue
 
 function dcs() {
-	docker-compose -p lbdemo -f ./docker-compose-prebuilt.yml $@
+    docker-compose -p lbdemo -f ./docker-compose-prebuilt.yml $@
 }
 
 
-WORK_DIR=$(readlink -e $(dirname "$0"))
+WORK_DIR=$(echo $(cd -P -- "$(dirname -- "$0")" && pwd -P))
 
-(test -d $WORK_DIR/env || virtualenv -p python2 $WORK_DIR/env )
+(test -d $WORK_DIR/env || virtualenv $WORK_DIR/env )
 set +u
 . $WORK_DIR/env/bin/activate
 set -u
 pip install -r $WORK_DIR/../docker/requirements.txt
 
 (
-	docker --version && docker ps >/dev/null && docker-compose --version
+    docker --version && docker ps >/dev/null && docker-compose --version
 ) || (
-	echo "Depended executable not found. Check the message above" && exit 1
+    echo "Depended executable not found. Check the message above" && exit 1
 ) &&
 
 
