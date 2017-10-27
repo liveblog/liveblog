34a7b31013bea1bd3e239d23ec0829ef5040d8e0
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-10-27 19:05:25 +0300

    fix: added date time language for amp.

--- server/liveblog/themes/template/filters.py
+++ server/liveblog/themes/template/filters.py
@@ -1,18 +1,31 @@
 import arrow
 from liveblog.blogs.app_settings import DEFAULT_THEME_DATE_FORMAT
+import logging
+
+logger = logging.getLogger('superdesk')
 
 
-def moment_date_filter(date, format=None):
-    """
-    Jinja2 filter for moment.js compatible dates.
-    :param date:
-    :param format:
-    :return: str
-    """
-    parsed = arrow.get(date)
-    # Workaround for "x" unsupported format
-    if format == 'x':
-        return parsed.timestamp
-    if not format:
-        format = DEFAULT_THEME_DATE_FORMAT
-    return parsed.format(format)
+def moment_date_filter_container(theme):
+    def moment_date_filter(date, format=None):
+        """
+        Jinja2 filter for moment.js compatible dates.
+        :param date:
+        :param format:
+        :return: str
+        """
+        settings = theme.get('settings', {})
+        parsed = arrow.get(date)
+        # Workaround for "x" unsupported format
+        if format == 'x':
+            return parsed.timestamp
+        if not format:
+            format = DEFAULT_THEME_DATE_FORMAT
+        formated = parsed.format(format)
+        try:
+            formated = parsed.format(format, settings.get('language', 'en'))
+        except Exception:
+            logger.info("language not supported")
+
+        return formated
+
+    return moment_date_filter
