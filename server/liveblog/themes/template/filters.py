5de09c539520f35d60774de91d22faa0f189ad24
Nistor Mihai <Mihai.Nistor@sourcefabric.org>
2017-11-02 21:19:38 +0200

    fix: final addten.

--- server/liveblog/themes/template/filters.py
+++ server/liveblog/themes/template/filters.py
@@ -1,4 +1,5 @@
 import arrow
+import datetime
 import re
 from settings import (DEFAULT_THEME_DATE_FORMAT, DEFAULT_THEME_TIMEZONE)
 import logging
@@ -38,11 +39,21 @@ def moment_date_filter_container(theme):
 
 
 def regaddten(obj):
+    print(obj)
     val = int(obj.group(1))
     return str(val + 10)
 
 
-def addten(datestr):
-    if datestr:
-        return re.sub(r'(\d{4})', regaddten, datestr)
-    return ''
+def addten(date):
+
+    if isinstance(date, datetime.datetime):
+        try:
+            date = date.replace(year=date.year + 10)
+        except ValueError:
+            # Leap day in a leap year, move date to February 28th
+            date = date.replace(year=date.year + 10, day=28)
+
+    if isinstance(date, str):
+        return re.sub(r'(\d{4})', regaddten, date)
+
+    return date
