ccabfd31f03644350c6ce84dac8faef1fcc5e1d7
GyanP <guptagyan58@gmail.com>
2018-03-12 18:32:51 +0530

    fix: Added a timeout to wait image to upload

--- client/app/scripts/liveblog-edit/directives/freetype-image.js
+++ client/app/scripts/liveblog-edit/directives/freetype-image.js
@@ -1,8 +1,8 @@
 import freetypeImageTpl from 'scripts/liveblog-edit/views/freetype-image.html';
 
-freetypeImage.$inject = ['$compile', 'modal', 'api', 'upload', 'superdesk', 'urls', 'notify'];
+freetypeImage.$inject = ['$compile', 'modal', 'api', 'upload', 'superdesk', 'urls', 'notify', '$timeout'];
 
-export default function freetypeImage($compile, modal, api, upload, superdesk, urls, notify) {
+export default function freetypeImage($compile, modal, api, upload, superdesk, urls, notify, $timeout) {
     return {
         restrict: 'E',
         templateUrl: freetypeImageTpl,
@@ -73,7 +73,9 @@ export default function freetypeImage($compile, modal, api, upload, superdesk, u
                         );
                     }, (progress) => {
                         $scope.progress.width = Math.round(progress.loaded / progress.total * 100.0);
-                        $scope.validation.imageUploaded = true;
+                        $timeout(function () {
+                            $scope.validation.imageUploaded = true;
+                        }, 3000);
                     }));
             };
 
