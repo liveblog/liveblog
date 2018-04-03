b8a8ac479a9f0e6076623161e813788af300a845
GyanP <guptagyan58@gmail.com>
2018-04-03 17:01:37 +0530

    fixed: test_if_check_max_active pass

--- server/liveblog/blogs/blogs_test.py
+++ server/liveblog/blogs/blogs_test.py
@@ -2,6 +2,7 @@ from liveblog.blogs import init_app
 from superdesk.tests import TestCase
 from superdesk import get_resource_service
 from superdesk.errors import SuperdeskApiError
+from settings import SUBSCRIPTION_LEVEL, SUBSCRIPTION_MAX_ACTIVE_BLOGS
 
 
 class BlogsTestCase(TestCase):
@@ -15,6 +16,9 @@ class BlogsTestCase(TestCase):
         self.assertEqual(get_resource_service('blogs')._check_max_active(increment), None)
 
     def test_if_check_max_active(self):
-        increment = 10
+        try:
+            increment = SUBSCRIPTION_MAX_ACTIVE_BLOGS[SUBSCRIPTION_LEVEL] + 5
+        except KeyError:
+            increment = 10
         with self.assertRaises(SuperdeskApiError):
             get_resource_service('blogs')._check_max_active(increment)
