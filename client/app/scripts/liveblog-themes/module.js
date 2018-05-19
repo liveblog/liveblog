7b3e53e0cb9bfbb628977b71f2a0a121c3b83356
Helmy Giacoman <helmygb@gmail.com>
2018-05-18 21:36:24 -0600

    LBSD-2199 Remove simple theme from the managment view

--- client/app/scripts/liveblog-themes/module.js
+++ client/app/scripts/liveblog-themes/module.js
@@ -150,7 +150,10 @@ import listTpl from 'scripts/liveblog-themes/views/list.ng1';
             },
             cannotRemove: function(theme) {
                 const hasChildren = self.themes.some((t) => t.extends === theme.name);
-                const systemThemes = ['angular', 'classic', 'default', 'amp', 'simple'];
+
+                // Removing simple theme https://dev.sourcefabric.org/browse/LBSD-2199
+                // const systemThemes = ['angular', 'classic', 'default', 'amp', 'simple'];
+                const systemThemes = ['angular', 'classic', 'default', 'amp'];
                 const isSystemTheme = systemThemes.indexOf(theme.name) !== -1;
 
                 return hasChildren || isSystemTheme;
