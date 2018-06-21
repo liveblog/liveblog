f21262393b5dee6e789b0af2bf41450a64d2df48
GyanP <guptagyan58@gmail.com>
2018-06-21 18:33:24 +0530

    added: two click solution for data privacy

--- server/liveblog/themes/themes_assets/default/js/theme/index.js
+++ server/liveblog/themes/themes_assets/default/js/theme/index.js
@@ -9,7 +9,8 @@ const handlers = require('./handlers'),
   view = require('./view'),
   pageview = require('./pageview'),
   localAnalytics = require('./local-analytics'),
-  adsManager = require('./ads-manager');
+  adsManager = require('./ads-manager'),
+  cookieEnabler = require('./cookies-enabler');
 
 require("iframe-resizer/js/iframeResizer.contentWindow.min.js");
 module.exports = {
@@ -22,8 +23,38 @@ module.exports = {
     viewmodel.init();
     localAnalytics.hit();
     pageview.init();
-
     adsManager.init();
+
+    COOKIES_ENABLER.init({
+        scriptClass: 'ce-script',
+        iframeClass: 'ce-iframe',
+
+        acceptClass: 'ce-accept',           
+        dismissClass: 'ce-dismiss',
+        disableClass: 'ce-disable',
+
+        bannerClass: 'ce-banner',
+        bannerHTML:'<a style="position: fixed; bottom: 0; left: 0; right: 0;'+
+        '  background-color: #dedede; padding: 1em; color: #232323; font-size: 1em;" '+
+        '  data-toggle="modal" data-target="#myModal">' + 
+        '<b>View Scocial Media</b></a>',
+        eventScroll: false,
+        scrollOffset: 200,
+
+        clickOutside: false,
+
+        cookieName: 'ce-cookie',
+        cookieDuration: '365',
+        wildcardDomain: true,
+
+        iframesPlaceholder: true,
+        iframesPlaceholderHTML:'',
+        iframesPlaceholderClass: 'ce-iframe-placeholder',
+            // Callbacks
+            onEnable: '',
+            onDismiss: '',
+            onDisable: ''
+          });
 
     view.updateTimestamps();
     setInterval(() => {
