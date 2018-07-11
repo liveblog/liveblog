923c26561e3906b91a81c85b7e9cf97d75725064
GyanP <guptagyan58@gmail.com>
2018-07-11 16:16:45 +0530

    Minor change

--- server/liveblog/syndication/tasks.py
+++ server/liveblog/syndication/tasks.py
@@ -54,15 +54,17 @@ def send_posts_to_consumer(self, syndication_out, action='created', limit=50, po
         array.reverse()
 
         for producer_post in array:
-            if producer_post.get('post_status') == 'open' or 'syndication_in' in producer_post.keys():
-                items = extract_post_items_data(producer_post)
-                post = extract_producer_post_data(producer_post)
-                # Force post_status for old posts
-                post['post_status'] = post_status
-                consumers.send_post(syndication_out, {
-                    'items': items,
-                    'post': post
-                }, action)
+            # if producer_post.get('post_status') == 'open' or 'syndication_in' in producer_post.keys():
+            if producer_post.get('post_status') == 'submitted' and 'syndication_in' not in producer_post.keys():
+-                continue
+            items = extract_post_items_data(producer_post)
+            post = extract_producer_post_data(producer_post)
+            # Force post_status for old posts
+            post['post_status'] = post_status
+            consumers.send_post(syndication_out, {
+                'items': items,
+                'post': post
+            }, action)
     except APIConnectionError as e:
         logger.warning('Unable to send posts to consumer: {}'.format(e))
         raise self.retry(exc=e, max_retries=SYNDICATION_CELERY_MAX_RETRIES, countdown=SYNDICATION_CELERY_COUNTDOWN)
