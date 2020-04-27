ef42d29298a7cce4d4b4e953f92af1b5b0d2604a
Helmy Giacoman <helmygb@gmail.com>
2020-04-27 17:43:26 +0200

    LBSD-2467 Default Theme reworked storage of gdpr consent. Cookie to localStorage

--- server/liveblog/themes/themes_assets/default/js/theme/gdpr.js
+++ server/liveblog/themes/themes_assets/default/js/theme/gdpr.js
@@ -1,12 +1,49 @@
 /* eslint-disable curly */
-import * as Cookies from 'js-cookie';
-
-const COOKIE_NAME = '__lb_consent_cookie__';
-const COOKIE_LIFE_DAYS = 365;
+const CONSENT_KEY = '__lb_consent_key__';
+const CONSENT_LIFE_DAYS = 365;
 const CONSENT_SUBJECTS_SELECTOR = 'template.lb_consent--awaiting';
 const CONSENT_PLACEHOLDER_TMPL = 'template#lb_consent--placeholder-tmpl';
 const PLACEHOLDER_SELECTOR = 'lb_consent--placeholder';
 
+
+class Storage {
+    static read = (name) => {
+        var itemStr = localStorage.getItem(name);
+
+        // if the item doesn't exist, return null
+        if (!itemStr)
+            return null;
+
+        var item = JSON.parse(itemStr);
+        var now = new Date();
+
+        // compare the expiry time of the item with the current time
+        if (now.getTime() > item.expiry) {
+            // If the item is expired, delete the item from storage
+            // and return null
+            localStorage.removeItem(name);
+            return null;
+        }
+
+        return item.value;
+    };
+
+    static write = (name, value, days) => {
+        var date = new Date();
+
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+
+        // `item` is an object which contains the original value
+        // as well as the time when it's supposed to expire
+        var item = {
+            value: value,
+            expiry: date.getTime(),
+        };
+
+        localStorage.setItem(name, JSON.stringify(item));
+    };
+
+}
 
 const wireCookiesEnabler = () => {
     if (isConsentGiven())
@@ -19,17 +56,14 @@ const wireCookiesEnabler = () => {
             btn.addEventListener('click', (ev) => {
                 ev.preventDefault();
 
-                Cookies.set(COOKIE_NAME, 'Y', {
-                    expires: COOKIE_LIFE_DAYS,
-                    sameSite: 'lax'
-                });
+                Storage.write(CONSENT_KEY, 'Y', 365);
                 checkAndHandlePlaceholders();
             });
         });
     }
 };
 
-const isConsentGiven = () => Cookies.get(COOKIE_NAME) === 'Y';
+const isConsentGiven = () => Storage.read(CONSENT_KEY) === 'Y';
 
 const getNodesAwaitingConsent = () => document.querySelectorAll(CONSENT_SUBJECTS_SELECTOR);
 
