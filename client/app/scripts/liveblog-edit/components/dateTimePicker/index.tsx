5503a4630fa3ef16f8053fbfb5a8a4633285baea
Helmy Giacoman <helmygb@gmail.com>
2020-09-16 15:52:32 +0200

    Implement custom styles for datetime picker according LB
    This is a combination of css styling for atlassian wrapper and for react-select
    inner components.
    
    LBSD-2442

--- client/app/scripts/liveblog-edit/components/dateTimePicker/index.tsx
+++ client/app/scripts/liveblog-edit/components/dateTimePicker/index.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import moment from 'moment';
 import { DateTimePicker as DTPicker } from '@atlaskit/datetime-picker';
 import { generateTimes } from './utils';
+import { Styles } from 'react-select/src/styles';
 
 interface IProps {
     datetime?: string;
@@ -10,6 +11,37 @@ interface IProps {
 
 // otherwise TS will complain with warnings
 const AtlasPicker = DTPicker as any;
+
+const selectStyles: Styles = {
+    control: (provided) => {
+        const styles = {
+            ...provided,
+            minHeight: '32px',
+        };
+
+        return styles;
+    },
+
+    valueContainer: (provided) => ({
+        ...provided,
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        paddingLeft: '3px',
+        paddingRight: '3px',
+    }),
+
+    input: (provided) => ({
+        ...provided,
+        paddingTop: '0px',
+        paddingBottom: '0px',
+        margin: '1px',
+    }),
+
+    singleValue: (provided) => ({
+        ...provided,
+        color: '#808080',
+    }),
+};
 
 export const DateTimePicker: React.FunctionComponent<IProps> = (props) => {
     const currentUtc = moment().utc();
@@ -29,6 +61,8 @@ export const DateTimePicker: React.FunctionComponent<IProps> = (props) => {
     };
 
     const times = generateTimes('01:00', 30, 'minutes');
+    const [isFocused, setFocused] = useState(false);
+    const containerClass = 'schedule-datetime-picker';
 
     return (
         <div className="flex-grid flex-grid--wrap-items flex-grid--small-1">
@@ -39,6 +73,17 @@ export const DateTimePicker: React.FunctionComponent<IProps> = (props) => {
                     timeIsEditable={true}
                     isInvalid={invalid}
                     times={times}
+                    innerProps={{
+                        className: `${containerClass}${isFocused ? '__hover' : ''}`,
+                    }}
+                    datePickerSelectProps={{
+                        styles: selectStyles,
+                    }}
+                    timePickerSelectProps={{
+                        styles: selectStyles,
+                    }}
+                    onFocus={() => setFocused(true)}
+                    onBlur={() => setFocused(false)}
                 />
 
                 {invalid ? <span>Please provide a valid date and time (in future)</span> : null}
