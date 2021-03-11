8564ced65da7afaf9910cdd58b7744a1ee5624d6
Helmy Giacoman <helmygb@gmail.com>
2021-03-11 12:26:23 +0100

    Fix error with cache_timeout
    This value is now expected in seconds instead of timedelta
    converting the value to seconds fixes the error

--- server/liveblog/themes/utils.py
+++ server/liveblog/themes/utils.py
@@ -9,6 +9,7 @@ def send_uploaded_static_file(app):
     def _inner(filename):
         cache_timeout = app.config['SEND_FILE_MAX_AGE_DEFAULT']
         static_folder = app.config['UPLOAD_THEMES_DIRECTORY']
-        return send_from_directory(static_folder, filename, cache_timeout=cache_timeout)
+        return send_from_directory(
+            static_folder, filename, cache_timeout=cache_timeout.total_seconds())
 
     return _inner
