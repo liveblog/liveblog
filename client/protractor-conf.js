4a6f1f6fe713e86510c73d37c4c4e9573d767be2
Helmy Giacoman <helmygb@gmail.com>
2022-02-03 11:16:19 +0100

    Adjust protractor configs

--- client/protractor-conf.js
+++ client/protractor-conf.js
@@ -1,10 +1,10 @@
-'use strict';
+
 
 var path = require('path');
 
 function getChromeOptions() {
     var chromeOptions = {
-        args: ['no-sandbox']
+        args: ['no-sandbox'],
     };
 
     if (process.env.CHROME_BIN) {
@@ -22,55 +22,55 @@ var config = {
     params: {
         baseBackendUrl: 'http://localhost:5000/api/',
         username: 'admin',
-        password: 'admin'
+        password: 'admin',
     },
 
     suites: {
         a: path.join(__dirname, '/spec/**/[a-f]*[Ss]pec.js'),
         b: path.join(__dirname, '/spec/**/[g-m]*[Ss]pec.js'),
-        c: path.join(__dirname, '/spec/**/[n-z]*[Ss]pec.js')
+        c: path.join(__dirname, '/spec/**/[n-z]*[Ss]pec.js'),
     },
 
     framework: 'jasmine2',
     jasmineNodeOpts: {
         showColors: true,
-        defaultTimeoutInterval: 200000
+        defaultTimeoutInterval: 200000,
     },
 
     capabilities: {
         browserName: 'chrome',
-        chromeOptions: getChromeOptions()
+        chromeOptions: getChromeOptions(),
     },
 
     onPrepare: function() {
         require('./node_modules/superdesk-core/spec/helpers/setup')({fixture_profile: 'test'});
         var reporters = require('jasmine-reporters');
 
-        by.addLocator('testId', function(value, parentElement) {
-            parentElement = parentElement || document;
-            var nodes = parentElement.querySelectorAll('[data-test-id]');
+        by.addLocator('testId', (value, parentElement) => {
+            const elem = parentElement || document;
+            var nodes = elem.querySelectorAll('[data-test-id]');
 
-            return Array.prototype.filter.call(nodes, function(node) {
-                return (node.getAttribute('data-test-id') === value);
-            });
+            return Array.prototype.filter.call(
+                nodes, (node) => (node.getAttribute('data-test-id') === value));
         });
 
         jasmine.getEnv().addReporter(
             new reporters.JUnitXmlReporter({
                 savePath: 'e2e-test-results',
-                consolidateAll: true
+                consolidateAll: true,
             })
         );
         function CustomReporter() {
             this.specDone = function(result) {
                 if (result.failedExpectations.length > 0) {
                     var name = result.fullName.split(' ');
+
                     console.log('at ' + name[0] + ': ' + result.description);
                 }
             };
         }
         jasmine.getEnv().addReporter(new CustomReporter());
-    }
+    },
 };
 
 if (process.env.SELENIUM_ADDRESS) {
