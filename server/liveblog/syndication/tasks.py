780e10d4b0b77b85497ae85de228c02242e0f116
Helmy Giacoman <helmygb@gmail.com>
2022-08-16 15:39:05 +0200

    Send posts one by task instead of whole batch
    Instead of sending all the posts in a single fat celery task, I've split them into several subtasks. The idea is that if one of the post fails to be created in the consumer side, the other posts can still have a chance to be sent and created in the consumer side even if some of them don't make it

--- server/liveblog/syndication/tasks.py
+++ server/liveblog/syndication/tasks.py
@@ -1,11 +1,11 @@
 import logging
+from celery import chain
 
 from flask import current_app as app
 from superdesk import get_resource_service
 from superdesk.celery_app import celery
 from superdesk.metadata.item import CONTENT_TYPE, ITEM_TYPE
 from superdesk.notification import push_notification
-
 from .exceptions import APIConnectionError
 from settings import SYNDICATION_CELERY_MAX_RETRIES, SYNDICATION_CELERY_COUNTDOWN
 from .utils import send_api_request
@@ -32,41 +32,84 @@ def send_post_to_consumer(self, syndication_out, producer_post, action='created'
 
 @celery.task(bind=True)
 def send_posts_to_consumer(self, syndication_out, action='created', limit=50, post_status='submitted'):
-    """Send latest blog post updates to consumers webhook."""
+    """
+    Sends latest blog post updates to consumers webhook.
+
+    When a consumer attaches to an existing blog, the producer sends a batch of posts that
+    should be created sequentially (in the consumer) to preserve the order of the posts.
+    To achieve this, we use a celery `chain` of subtasks. Subtasks in the chain must not
+    raise any exception in order to continue the subsequent task and the next post is sent.
+    """
     from .utils import extract_post_items_data, extract_producer_post_data
-    consumers = get_resource_service('consumers')
-    blog_id = syndication_out['blog_id']
+
     posts_service = get_resource_service('posts')
+
+    blog_id = syndication_out['blog_id']
     lookup = {'blog': blog_id, ITEM_TYPE: CONTENT_TYPE.COMPOSITE, 'deleted': False, 'post_status': 'open'}
 
     posts = posts_service.find(lookup)
+    posts = posts.sort('order', -1)  # order to latest first
 
-    # sort by order to get the latest first
-    posts = posts.sort('order', -1)
+    array = []
+    for element in posts:
+        array.append(element)
 
+        if len(array) >= limit:
+            break
+
+    subtasks = []
+    array.reverse()
+    for producer_post in array:
+        is_repeated_syndication = 'repeat_syndication' in producer_post.keys()
+        if is_repeated_syndication:
+            continue
+
+        items = extract_post_items_data(producer_post)
+        post = extract_producer_post_data(producer_post)
+
+        # Force post_status for old posts
+        post['post_status'] = post_status
+
+        post_data = {
+            'items': items,
+            'post': post
+        }
+        subtask_args = [syndication_out, post_data, action]
+
+        # Because we are running the subtask in a chained mode, we need to provide
+        # the initial param or result of other tasks as None. Read more here:
+        # https://docs.celeryq.dev/en/stable/userguide/canvas.html#chains
+        # This is required as we need to keep the order of the posts
+        if len(subtasks) == 0:
+            subtask_args.insert(0, None)
+
+        send_internal = send_single_post_subtask.subtask(args=subtask_args)
+        subtasks.append(send_internal)
+
+    job = chain(subtasks)
+    job.apply_async()
+
+
+@celery.task(bind=True)
+def send_single_post_subtask(self, chained_result, syndication_out, post_data, action):
     try:
-        array = []
-        for element in posts:
-            array.append(element)
-            if len(array) >= limit:
-                break
+        consumers = get_resource_service('consumers')
+        consumers.send_post(syndication_out, post_data, action)
+    except Exception as err:
+        # once retry limit is reached, let's return instead of raising an exception
+        # so the next subtask (post) will be also sent
+        task_retries = SYNDICATION_CELERY_MAX_RETRIES + 1
+        if task_retries == self.request.retries:
+            post_id = post_data['post']['_id']
+            consumer_id = syndication_out['consumer_id']
+            consumer_blog_id = syndication_out['consumer_blog_id']
 
-        array.reverse()
-        for producer_post in array:
-            # Don't forward syndicated posts
-            if 'repeat_syndication' in producer_post.keys():
-                continue
-            items = extract_post_items_data(producer_post)
-            post = extract_producer_post_data(producer_post)
-            # Force post_status for old posts
-            post['post_status'] = post_status
-            consumers.send_post(syndication_out, {
-                'items': items,
-                'post': post
-            }, action)
-    except APIConnectionError as e:
-        logger.warning('Unable to send posts to consumer: {}'.format(e))
-        raise self.retry(exc=e, max_retries=SYNDICATION_CELERY_MAX_RETRIES, countdown=SYNDICATION_CELERY_COUNTDOWN)
+            logger.error(
+                "Unable to send post '%s' to consumer '%s' and blog '%s'. Exception %s",
+                post_id, consumer_id, consumer_blog_id, err)
+            return
+
+        raise self.retry(exc=err, max_retries=task_retries, countdown=SYNDICATION_CELERY_COUNTDOWN)
 
 
 @celery.task(bind=True)
