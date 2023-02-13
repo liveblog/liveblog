17d849f8e407b103edcbe01cd41ea847c3cb7f92
Helmy Giacoman <helmygb@gmail.com>
2023-02-13 18:23:08 +0100

    Implement facebook embed render
    LBSD-2697

--- client/app/scripts/liveblog-edit/components/itemEmbedRender.tsx
+++ client/app/scripts/liveblog-edit/components/itemEmbedRender.tsx
@@ -1,25 +1,37 @@
 import React from 'react';
-import { TwitterEmbed as TwttEmbed } from 'react-social-media-embed';
+import { FacebookEmbed, TwitterEmbed } from 'react-social-media-embed';
 import { ItemEmbedGeneric } from './itemEmbedGeneric';
 
 interface IProps {
     text: string;
     title: string;
     description: string;
+    url: string;
     original_url: string; // eslint-disable-line camelcase
     provider_name: string; // eslint-disable-line camelcase
 }
 
-// doing this because missing `style` property typing
-const TwitterEmbed = TwttEmbed as any;
-
 export const ItemEmbedRender: React.FunctionComponent<IProps> = (props) => {
     switch (props.provider_name) {
+    case 'Facebook':
+        return (
+            <FacebookEmbed
+                url={props.url}
+                linkText={props.title}
+                // @ts-ignore:next-line
+                style={{
+                    maxWidth: 550,
+                    minHeight: 600,
+                }}
+                width="100%"
+            />
+        );
     case 'Twitter':
         return (
             <TwitterEmbed
                 linkText={props.title}
                 url={props.original_url}
+                // @ts-ignore:next-line
                 style={{
                     maxWidth: 550,
                     minHeight: 600,
