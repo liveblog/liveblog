4eda2ce08fd2c77f5bb960e17b2302aec309a9bb
Helmy Giacoman <helmygb@gmail.com>
2023-04-26 15:50:49 +0200

    Revert "Remove fixes to run scenario on test instances"
    This reverts commit 78e888a705b496558a93eb0d499c9e7cbd809ec3.

--- client/app/scripts/sir-trevor-blocks/image-block.js
+++ client/app/scripts/sir-trevor-blocks/image-block.js
@@ -235,7 +235,7 @@ export default function imageBlock(SirTrevor, config) {
         retrieveData: function() {
             const data = this.getData();
 
-            if (data.media === undefined) return {};
+            if (data.media === undefined) return null;
 
             return {
                 media: data.media,
