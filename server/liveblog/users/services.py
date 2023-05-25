c856e67dcfd3b29a18e48f0e39da0f1c725fc9e9
Helmy Giacoman <helmygb@gmail.com>
2023-05-25 17:26:00 +0200

    Hide users information if env var enabled
    Given that we're creating a role with more powers for demo purposes (super editor), we need to care about the privacy of others users and do not expose data that can be sensitive. For this, we are implementing a new environment variable that if it's enabled, then the fields containing sensitive information will be replaced with a dummy string. This env var is False by default.
    
    LBSD-2674

---
+++ server/liveblog/users/services.py
@@ -0,0 +1,31 @@
+import flask
+from flask import current_app as app
+from superdesk.users.services import DBUsersService
+
+
+class LiveBlogUserService(DBUsersService):
+    """
+    Exnteds superdesk.users default app to add some additional functionality
+    only concerning Live Blog, like hiding users' sensitive information for users
+    that do not have enough permissions to do so.
+    """
+
+    def on_fetched(self, document):
+        super().on_fetched(document)
+
+        for doc in document['_items']:
+            self.__hide_sensitive_data(doc)
+
+    def on_fetched_item(self, doc):
+        super().on_fetched_item(doc)
+        self.__hide_sensitive_data(doc)
+
+    def __hide_sensitive_data(self, doc):
+        """Set default fields for users"""
+
+        if flask.g.user['_id'] == doc['_id']:
+            return
+
+        if app.config['HIDE_USERS_SENSITIVE_DATA']:
+            doc['email'] = 'hidden'
+            doc['first_name'] = 'hidden'
