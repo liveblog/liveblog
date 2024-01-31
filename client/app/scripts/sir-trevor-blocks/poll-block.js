245b6dfb9aefab7db840d1ee6258c4453ec72845
kabiru <mwangikabiru21@gmail.com>
2024-01-31 16:01:39 +0300

    Static Editor UI

--- client/app/scripts/sir-trevor-blocks/poll-block.js
+++ client/app/scripts/sir-trevor-blocks/poll-block.js
@@ -1,22 +1,19 @@
-/* eslint-disable */
-// @ts-nocheck
-
 import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-
-class PollComponent extends React.Component {
-  render() {
-    return <div>Create Poll</div>;
-  }
-}
+import ReactDOM from 'react-dom';
+import PollComponent from './components/pollComponent.tsx';
 
 export default function pollBlock(SirTrevor, config) {
-  return SirTrevor.Block.extend({
-    type: 'poll',
-    title: 'Poll',
-    icon_name: 'poll',
-    editorHTML: function() {
-      return ReactDOMServer.renderToString(<PollComponent />);
-    }
-  });
-};
+    return SirTrevor.Block.extend({
+        type: 'poll',
+        title: 'Poll',
+        icon_name: 'poll',
+        editorHTML: function() {
+            return '<div class="poll_block_container"></div>';
+        },
+        onBlockRender: function() {
+            const container = this.$('.poll_block_container')[0];
+
+            ReactDOM.render(<PollComponent />, container);
+        },
+    });
+}
