cac8f03207770b5706fdfb386235d8694128f288
kabiru <mwangikabiru21@gmail.com>
2024-03-12 10:55:37 +0300

    Persist user votes in storage and update UI accordingly if user has already voted on a poll

--- server/liveblog/themes/themes_assets/default/js/theme/polls.js
+++ server/liveblog/themes/themes_assets/default/js/theme/polls.js
@@ -1,16 +1,33 @@
 'use strict';
 
+import {Storage} from './common/storage';
 var helpers = require('./helpers');
 
-const apiHost = LB.api_host.match(/\/$/i) ? LB.api_host : LB.api_host + '/';
-const pollsEndpoint = `${apiHost}api/client_polls/`;
+const POLLS_KEY = '__lb_polls_data__';
+const POLLS_EXPIRY_DAYS = 365;
 
-function placeVote(event) {
-  const selectedOption = event.detail.selectedOption;
-  const selectedPoll = event.detail.selectedPoll;
+const apiHost = LB.api_host.match(/\/$/i) ? LB.api_host : LB.api_host + '/';
+const blogId = LB.blog._id;
+
+/**
+ * Updates the UI with the new total votes and hides the vote button.
+ */
+function updatePollUI(totalVotes, selectedPoll) {
+  const totalVotesUpdatedId = `total-votes-updated-${selectedPoll}`;
   const totalVotesId = `total-votes-${selectedPoll}`;
   const voteButtonId = `vote-button-${selectedPoll}`;
-  const pollEndpoint = pollsEndpoint + selectedPoll;
+
+  document.getElementById(totalVotesUpdatedId).innerText = totalVotes;
+  document.getElementById(totalVotesId).style.display = "none";
+  document.getElementById(voteButtonId).style.display = "none";
+}
+
+/**
+ * Places a vote for a poll and updates the UI with the new total votes.
+ */
+function placeVote(event) {
+  const { selectedOption, selectedPoll } = event.detail;
+  const pollEndpoint = `${apiHost}api/client_polls/${selectedPoll}`;
 
   helpers.get(pollEndpoint)
     .then((poll) => {
@@ -20,12 +37,49 @@ function placeVote(event) {
 
       helpers.patch(pollEndpoint, data, etag)
         .then((updatedPoll) => {
-          // Update the votes count on the readers UI and hide button for now 
           let totalVotes = updatedPoll.poll_body.answers.reduce((acc, ans) => acc + ans.votes, 0);
-          document.getElementById(totalVotesId).innerText = totalVotes;
-          document.getElementById(voteButtonId).style.display = "none";
+          updatePollUI(totalVotes, selectedPoll);
+          persistVote(selectedPoll, selectedOption);
       });
     });
+}
+
+/**
+ * Persists the user's vote for a poll in local storage.
+ */
+function persistVote(selectedPoll, selectedOption) {
+  let pollsData = Storage.read(POLLS_KEY) || {};
+
+  pollsData[blogId] = {
+    ...pollsData[blogId],
+    [selectedPoll]: selectedOption,
+  };
+
+  Storage.write(POLLS_KEY, pollsData, POLLS_EXPIRY_DAYS);
+}
+
+/**
+ * Checks for existing votes in local storage and updates the UI accordingly by
+ * hiding the vote button for polls that have already been voted on and
+ * selecting the users option for a visual cue.
+ */
+function checkExistingVotes() {
+  const pollsData = Storage.read(POLLS_KEY) || {};
+  const blogPolls = pollsData[blogId];
+
+  if(blogPolls) {
+    Object.entries(blogPolls).forEach(([pollId, selectedOption]) => {
+      const voteButton = document.getElementById(`vote-button-${pollId}`);
+      const selectedOptionInput = document.getElementById(`${pollId}-answer-${selectedOption}`);
+
+      if (voteButton && selectedOptionInput) {
+        voteButton.style.display = "none";
+        selectedOptionInput.checked = true;
+      } else {
+        console.warn(`Elements for poll ID ${pollId} not found.`);
+      }
+    })
+  }
 }
 
 module.exports = {
@@ -33,5 +87,6 @@ module.exports = {
         document.addEventListener('place_vote', function (e) {
             placeVote(e);
         });
+        checkExistingVotes();
     },
 };
