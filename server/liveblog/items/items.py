04e11687928baed016bbecf6d9c0dcd223064b4a
Helmy Giacoman <helmygb@gmail.com>
2024-04-04 11:58:35 +0200

    Remove useless code
    The modifications to metadata (conversion of version, width, and height to strings) after the initial call to `set_filemeta(doc, metadata)` were not persisted back into doc. These code seemed to be doing nothing and didn't affect anything once it is removed.
    
    LBSD-2772

--- server/liveblog/items/items.py
+++ server/liveblog/items/items.py
@@ -147,20 +147,16 @@ class ItemsService(ArchiveService):
 
     def on_create(self, docs):
         super().on_create(docs)
+
         for doc in docs:
             update_dates_for(doc)
             doc["original_creator"] = str(get_user().get("_id"))
-            if doc.get("item_type"):
-                if doc["item_type"] == "embed":
-                    metadata = doc["meta"]
-                    set_filemeta(doc, metadata)
-                    if get_filemeta(doc, "version"):
-                        metadata["version"] = str(metadata.get("version"))
-                    if get_filemeta(doc, "width"):
-                        metadata["width"] = str(metadata.get("width"))
-                    if get_filemeta(doc, "height"):
-                        metadata["height"] = str(metadata.get("height"))
-                    self.set_embed_metadata(doc)
+
+            is_item_embed_type = doc.get("item_type") == "embed"
+            if is_item_embed_type:
+                set_filemeta(doc, doc["meta"])
+                self.set_embed_metadata(doc)
+
         return doc
 
     def on_created(self, docs):
