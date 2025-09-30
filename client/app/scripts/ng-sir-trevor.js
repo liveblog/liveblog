761da1a5f567008bb2517e29be28a4712f4927de
Helmy Giacoman <helmygb@gmail.com>
2025-09-30 18:26:42 +0200

    Move german-quotation.ts to helpers directory

--- client/app/scripts/ng-sir-trevor.js
+++ client/app/scripts/ng-sir-trevor.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 import _ from 'lodash';
-import GermanQuotation from './german-quotation';
+import GermanQuotation from './sir-trevor-blocks/helpers/german-quotation';
 
 export default angular
     .module('SirTrevor', [])
